311c0bbe506d446359d61589541303a5b557073b
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-02-07 09:02:33 +0200

    Add new excercises about Arrays

---
+++ C# Fundamentals/Arrays.MoreExercises/Arrays.MoreExercises/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Arrays.MoreExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
