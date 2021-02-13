66876f88521957298821a5ea08359f1c52e6cc3e
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-02-13 12:46:53 +0200

    HouseParty Problem
    The problem is solved. I used lists and methods about the different cases.

---
+++ C# Fundamentals/Lists.Exercise/03.HouseParty/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.HouseParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> guests = new List<string>();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] command = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                string name = command[0];
+                string action = string.Join(' ', command.Skip(1));
+
+                switch (action)
+                {
+                    case "is going!":
+                        if (CheckTheName(guests, name))
+                        {
+                            Console.WriteLine($"{name} is already in the list!");
+                        }
+                        else
+                        {
+                            guests.Add(name);
+                        }
+
+                        break;
+                    case "is not going!":
+                        if (!CheckTheName(guests, name))
+                        {
+                            Console.WriteLine($"{name} is not in the list!");
+                        }
+                        else
+                        {
+                            guests.Remove(name);
+                        }
+
+                        break;
+
+                    default:
+                        throw new InvalidOperationException();
+                }
+            }
+
+            PrintResult(guests);
+        }
+
+        private static void PrintResult(List<string> guests)
+        {
+            Console.WriteLine(string.Join(Environment.NewLine, guests));
+        }
+
+        private static bool CheckTheName(List<string> guests, string name)
+        {
+            return guests.Contains(name);
+        }
+    }
+}
