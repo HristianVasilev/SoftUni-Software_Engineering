341e25931886fa8969f3d662417acc0efa9a1b2a
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-02-13 14:45:14 +0200

    CardsGame
    Problem solved! It was very easy. :100:

--- C# Fundamentals/Lists.Exercise/06.CardsGame/Program.cs
+++ C# Fundamentals/Lists.Exercise/06.CardsGame/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace _06.CardsGame
 {
@@ -6,7 +8,44 @@ namespace _06.CardsGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            List<int> firstHand = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            List<int> secondHand = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+            while (firstHand.Count > 0 && secondHand.Count > 0)
+            {
+                int firstCard = firstHand[0];
+                int secondCard = secondHand[0];
+
+                firstHand.RemoveAt(0);
+                secondHand.RemoveAt(0);
+
+                if (firstCard > secondCard)
+                {
+                    firstHand.Add(firstCard);
+                    firstHand.Add(secondCard);
+                }
+                else if(firstCard < secondCard)
+                {
+                    secondHand.Add(secondCard);
+                    secondHand.Add(firstCard);
+                }
+            }
+
+            string winner;
+            int totalResult;
+
+            if (secondHand.Count == 0)
+            {
+                winner = "First";
+                totalResult = firstHand.Sum();
+            }
+            else
+            {
+                winner = "Second";
+                totalResult = secondHand.Sum();
+            }
+
+            Console.WriteLine($"{winner} player wins! Sum: {totalResult}");
         }
     }
 }
