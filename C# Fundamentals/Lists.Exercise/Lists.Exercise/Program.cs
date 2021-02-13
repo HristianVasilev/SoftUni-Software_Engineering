a05707c7453cea5545644d45e5b509b355a7da58
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-02-13 12:07:15 +0200

    Exercise about lists

---
+++ C# Fundamentals/Lists.Exercise/Lists.Exercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lists.Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
