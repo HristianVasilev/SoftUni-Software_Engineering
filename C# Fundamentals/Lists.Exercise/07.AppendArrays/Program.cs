b0c515ecb1617375367c3c2bbee91e665a09dc6b
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-02-13 15:16:02 +0200

    AppendArrays
    Problem solved :100:

---
+++ C# Fundamentals/Lists.Exercise/07.AppendArrays/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _07.AppendArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> output = new List<int>();
+            string input = Console.ReadLine();
+            string[] arrays = input.Split('|', StringSplitOptions.RemoveEmptyEntries).Reverse().ToArray();
+
+            for (int i = 0; i < arrays.Length; i++)
+            {
+                int[] currentArr = arrays[i].Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+                output.AddRange(currentArr);
+            }
+
+            Console.WriteLine(string.Join(' ',output));
+        }
+    }
+}
