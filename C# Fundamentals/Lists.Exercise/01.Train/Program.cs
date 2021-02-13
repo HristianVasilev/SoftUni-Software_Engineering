bde53b2ee8c90d663fdc6567ccddd3956a060c44
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-02-13 12:19:14 +0200

    TrainProblemSolved

---
+++ C# Fundamentals/Lists.Exercise/01.Train/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Train
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> wagons = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+            int maxCapacityOfWagon = int.Parse(Console.ReadLine());
+
+            string input = string.Empty;
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string[] tokens = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                int passengers;
+
+                if (tokens.Length > 1 && tokens[0] == "Add")
+                {
+                    passengers = int.Parse(tokens[1]);
+                    wagons.Add(passengers);
+                }
+                else
+                {
+                    passengers = int.Parse(tokens[0]);
+
+                    FitPassengers(wagons, maxCapacityOfWagon, passengers);
+                }
+            }
+
+            Console.WriteLine(string.Join(' ', wagons));
+        }
+
+        private static void FitPassengers(List<int> wagons, int maxCapacityOfWagon, int passengers)
+        {
+            for (int i = 0; i < wagons.Count; i++)
+            {
+                if (wagons[i] + passengers <= maxCapacityOfWagon)
+                {
+                    wagons[i] += passengers;
+                    return;
+                }
+            }
+        }
+    }
+}
