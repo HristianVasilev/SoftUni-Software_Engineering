5a065698373c1b779a984bcef1f73bb7127cb636
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-02-14 17:50:26 +0200

    Messaging
    problem solved :100:

---
+++ C# Fundamentals/Lists.MoreExercises/01.Messaging/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Messaging
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> output = new List<string>();
+
+            List<string> numbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).ToList();
+            string text = Console.ReadLine();
+
+            foreach (var number in numbers)
+            {
+                int index = SumOfDigits(number);
+
+                if (index > text.Length - 1)
+                {
+                    index = index % text.Length;
+                }
+
+                string character = text.Substring(index, 1);
+                text = text.Remove(index, 1);
+                output.Add(character);
+            }
+
+            Console.WriteLine(string.Join("", output));
+        }
+
+        private static int SumOfDigits(string number)
+        {
+            return number.Sum(x => int.Parse(x.ToString()));
+        }
+    }
+}
