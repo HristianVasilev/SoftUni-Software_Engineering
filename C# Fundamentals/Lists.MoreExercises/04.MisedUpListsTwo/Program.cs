4f3286ffed9df3494628012e79515cc08ca00212
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-02-16 07:30:30 +0200

    ready

---
+++ C# Fundamentals/Lists.MoreExercises/04.MisedUpListsTwo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.MisedUpListsTwo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> firstList = new Queue<int>(Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+            Stack<int> secondList = new Stack<int>(Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+
+            List<int> result = new List<int>();
+
+            while (secondList.Count > 0)
+            {
+                result.Add(firstList.Dequeue());
+                result.Add(secondList.Pop());
+            }
+
+            int lowerLimit = firstList.Min();
+            int higherLimit = firstList.Max();
+
+
+            Console.WriteLine(string.Join(' ',result.Where(x => x > lowerLimit && x < higherLimit).OrderBy(n => n)));
+        }
+    }
+}
---
+++ C# Fundamentals/Lists.MoreExercises/04.MixedUpLists/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.MixedUpLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] firstList = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+            int[] secondList = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            List<int> result = new List<int>();
+
+            sbyte firstCounter = 0;
+            sbyte secondCounter = (sbyte)(secondList.Length - 1);
+
+            while (secondCounter >= 0)
+            {
+                result.Add(firstList[firstCounter++]);
+
+                result.Add(secondList[secondCounter--]);
+            }
+
+            var range = firstList.TakeLast(2).ToArray();
+
+            int lowerLimit = range.Min();
+            int higherLimit = range.Max();
+
+            result = result.Where(x => x > lowerLimit && x < higherLimit).OrderBy(n => n).ToList();
+
+            Console.WriteLine(string.Join(' ', result));
+        }
+    }
+}
