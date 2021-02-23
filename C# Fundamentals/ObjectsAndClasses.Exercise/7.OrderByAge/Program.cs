4c74adc587530ef8f8c1112d19f019b6c88a8c0d
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-02-23 08:42:12 +0200

    Solution update

---
+++ C# Fundamentals/ObjectsAndClasses.Exercise/7.OrderByAge/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _7.OrderByAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Person> people = new List<Person>();
+
+            string input = string.Empty;
+            while ((input = Console.ReadLine()) != "End")
+            {
+                string[] personArgs = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string name = personArgs[0];
+                int ID = int.Parse(personArgs[1]);
+                int age = int.Parse(personArgs[2]);
+
+                Person person = new Person(name, ID, age);
+                people.Add(person);
+            }
+
+            string allPeople = GetAllPeopleOrderByAge(ref people);
+            Console.WriteLine(allPeople);
+        }
+
+        private static string GetAllPeopleOrderByAge(ref List<Person> people)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var person in people.OrderBy(x => x.Age))
+            {
+                sb.AppendLine(person.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+
+    class Person
+    {
+        public Person(string name, int ID, int age)
+        {
+            this.Name = name;
+            this.ID = ID;
+            this.Age = age;
+        }
+
+        public string Name { get; private set; }
+        public int ID { get; private set; }
+        public int Age { get; private set; }
+
+        public override string ToString()
+        {
+            return $"{this.Name} with ID: {this.ID} is {this.Age} years old.";
+        }
+    }
+}
---
+++ C# Fundamentals/ObjectsAndClasses.MoreExercises/1.CompanyRoster/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1.CompanyRoster
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>();
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] employeeArgs = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string name = employeeArgs[0];
+                decimal salary = decimal.Parse(employeeArgs[1]);
+                string department = employeeArgs[2];
+
+                Employee employee = new Employee(name, salary, department);
+                employees.Add(employee);
+            }
+
+            GetDepartmentWithHighesAvgSalary(ref employees);
+
+        }
+
+        private static void GetDepartmentWithHighesAvgSalary(ref List<Employee> employees)
+        {
+            var groupedCollection = employees.GroupBy(x => x.Department).OrderByDescending(x => x.Average(s => s.Salary)).ToList();
+         
+            IGrouping<string,Employee> departmentInfo = groupedCollection.FirstOrDefault();
+
+            if (departmentInfo is null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            var departmentEmployees = departmentInfo.Select(x => x).OrderByDescending(s => s.Salary).ToArray();
+
+            Console.WriteLine($"Highest Average Salary: {departmentInfo.Key}");
+
+            foreach (var employee in departmentEmployees)
+            {
+                Console.WriteLine(employee);
+            }
+        }
+    }
+
+
+    class Employee
+    {
+        public Employee(string name, decimal salary, string department)
+        {
+            this.Name = name;
+            this.Salary = salary;
+            this.Department = department;
+        }
+
+        public string Name { get; private set; }
+        public decimal Salary { get; private set; }
+        public string Department { get; private set; }
+
+        public override string ToString()
+        {
+            return $"{this.Name} {this.Salary:f2}";
+        }
+    }
+}
