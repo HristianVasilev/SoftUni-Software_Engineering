4e48b7f7b5a204955d73087db177e61e54d0fbf4
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-02-25 19:09:33 +0200

    MidExam 8 aug 19
    1st problem solved 100/100

---
+++ C# Fundamentals/MidExamPreparation/01.FundamentalsMidExam/Programs/MidExam6Aug19/MidExam6Aug19/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace MidExam6Aug19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int daysOfPlunder = int.Parse(Console.ReadLine());
+            int dailyPlunder = int.Parse(Console.ReadLine());
+            double expectedPlunder = double.Parse(Console.ReadLine());
+
+            double totalPlunder = GetPlunders(daysOfPlunder, dailyPlunder);
+
+            if (totalPlunder >= expectedPlunder)
+            {
+                Console.WriteLine($"Ahoy! {totalPlunder:f2} plunder gained.");
+            }
+            else
+            {
+                double percentage = totalPlunder / expectedPlunder * 100;
+
+                Console.WriteLine($"Collected only {percentage:f2}% of the plunder.");
+            }
+
+
+        }
+
+        private static double GetPlunders(int daysOfPlunder, int dailyPlunder)
+        {
+            double totalPlunder = 0;
+
+            for (int i = 1; i <= daysOfPlunder; i++)
+            {
+                totalPlunder += dailyPlunder;
+
+                if (i % 3 == 0)
+                {
+                    totalPlunder += (dailyPlunder / 2.0);
+                }
+
+                if (i % 5 == 0)
+                {
+                    totalPlunder *= 0.70;
+                }
+            }
+
+            return totalPlunder;
+        }
+    }
+}
