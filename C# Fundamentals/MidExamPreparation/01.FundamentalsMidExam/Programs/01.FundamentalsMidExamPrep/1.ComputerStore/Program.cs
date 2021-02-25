b76e149cbac81fbd1a44fa09ab5846f6b4c1e4f8
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-02-25 17:21:46 +0200

    problem 2 solved

--- C# Fundamentals/MidExamPreparation/01.FundamentalsMidExam/Programs/01.FundamentalsMidExamPrep/1.ComputerStore/Program.cs
+++ C# Fundamentals/MidExamPreparation/01.FundamentalsMidExam/Programs/01.FundamentalsMidExamPrep/1.ComputerStore/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 
 namespace _1.ComputerStore
 {
@@ -6,18 +7,49 @@ namespace _1.ComputerStore
     {
         static void Main(string[] args)
         {
+            decimal totalPriceWithoutTaxes = 0m;
+
             string input = Console.ReadLine();
 
             while (input != "special" && input != "regular")
             {
+                decimal price = decimal.Parse(input);
 
+                if (price < 0)
+                {
+                    Console.WriteLine("Invalid price!");
+                    input = Console.ReadLine();
+                    continue;
+                }
 
+                totalPriceWithoutTaxes += price;
 
                 input = Console.ReadLine();
             }
 
+            decimal taxes = totalPriceWithoutTaxes * 0.2m;
+            decimal totalPrice = totalPriceWithoutTaxes + taxes;
 
+            if (input == "special")
+            {
+                totalPrice *= 0.9m;
+            }
 
+            if (totalPrice == 0)
+            {
+                Console.WriteLine("Invalid order!");
+                return;
+            }
+
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Congratulations you've just bought a new computer!");
+            sb.AppendLine($"Price without taxes: {totalPriceWithoutTaxes:f2}$");
+            sb.AppendLine($"Taxes: {taxes:f2}$");
+            sb.AppendLine($"-----------");
+            sb.AppendLine($"Total price: {totalPrice:f2}$");
+
+            Console.WriteLine(sb.ToString().TrimEnd());
         }
+
     }
 }
---
+++ C# Fundamentals/MidExamPreparation/01.FundamentalsMidExam/Programs/01.FundamentalsMidExamPrep/2.TheLift/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Linq;
+
+namespace _2.TheLift
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int wagonCapacity = 4;
+
+            int peopleWaiting = int.Parse(Console.ReadLine());
+
+            int[] wagons = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).ToArray();
+
+            for (int i = 0; i < wagons.Length; i++)
+            {
+                while (wagons[i] < 4 && peopleWaiting > 0)
+                {
+                    wagons[i]++;
+                    peopleWaiting--;
+                }
+            }
+
+            if (peopleWaiting == 0 && wagons.Any(x => x != 4))
+            {
+                Console.WriteLine("The lift has empty spots!");
+            }
+
+            if (peopleWaiting != 0)
+            {
+                Console.WriteLine($"There isn't enough space! {peopleWaiting} people in a queue!");
+            }
+
+            Console.WriteLine(string.Join(' ',wagons));
+        }
+    }
+}
