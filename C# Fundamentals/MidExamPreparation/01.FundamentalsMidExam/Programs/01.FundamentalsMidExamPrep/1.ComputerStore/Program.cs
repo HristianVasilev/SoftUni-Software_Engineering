9c3f77f9f1a0885a1abd86cc2a129559a3861151
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-02-25 16:52:13 +0200

    mid exam prep

---
+++ C# Fundamentals/MidExamPreparation/01.FundamentalsMidExam/Programs/01.FundamentalsMidExamPrep/1.ComputerStore/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _1.ComputerStore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            while (input != "special" && input != "regular")
+            {
+
+
+
+                input = Console.ReadLine();
+            }
+
+
+
+        }
+    }
+}
