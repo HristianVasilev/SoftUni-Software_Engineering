22f99f050c19b9fd084d323b94417a495fd16d77
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-03-04 14:23:19 +0200

    Associative Arrays
    Problem 4th successfully solved!

---
+++ C# Fundamentals/AssociativeArrays.Lab/4.Largest3Numbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace _4.Largest3Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            var collection = numbers.OrderByDescending(n => n).Take(3);
+
+            Console.WriteLine(string.Join(' ',collection));
+        }
+    }
+}
