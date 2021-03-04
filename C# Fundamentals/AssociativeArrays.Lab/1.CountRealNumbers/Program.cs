55ae478cb4ed003344988eaff405a8d01762b4c9
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-03-04 09:07:38 +0200

    Associative arrays
    First problem successfully solved!

---
+++ C# Fundamentals/AssociativeArrays.Lab/1.CountRealNumbers/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _1.CountRealNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            Dictionary<int, int> stat = new Dictionary<int, int>();
+
+            int[] distinctNumbers = numbers.Distinct().OrderBy(n => n).ToArray();
+
+            foreach (int num in distinctNumbers)
+            {
+                stat.Add(num, numbers.Count(n => n.Equals(num)));
+            }
+
+            Print(ref stat);
+        }
+
+        private static void Print(ref Dictionary<int, int> stat)
+        {
+            foreach (var item in stat)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
