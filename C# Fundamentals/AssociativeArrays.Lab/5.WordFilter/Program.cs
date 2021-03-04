24fd4f8b622336a89573a140391eaca84e910765
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-03-04 16:39:58 +0200

    Associative Arrays
    5th problem is successfully solved!

---
+++ C# Fundamentals/AssociativeArrays.Lab/5.WordFilter/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace _5.WordFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            var collection = words.Where(w => w.Length % 2 == 0);
+
+            Console.WriteLine(string.Join(Environment.NewLine, collection));
+        }
+    }
+}
