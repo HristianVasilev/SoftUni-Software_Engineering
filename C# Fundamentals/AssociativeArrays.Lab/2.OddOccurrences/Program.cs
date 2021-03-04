bb398e84b7548ee4c78985d3014e41e7b54516f0
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-03-04 09:16:51 +0200

    Associative Arrays
    2nd problem from lab - successfully solved!

---
+++ C# Fundamentals/AssociativeArrays.Lab/2.OddOccurrences/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2.OddOccurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> statistic = new Dictionary<string, int>();
+
+            string[] elements = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var item in elements)
+            {
+                string element = item.ToLower();
+
+                if (!statistic.ContainsKey(element))
+                {
+                    statistic.Add(element, 0);
+                }
+
+                statistic[element]++;
+            }
+
+            var collection = statistic.Where(v => v.Value % 2 != 0).Select(kvp => kvp.Key).ToList();
+
+            Console.WriteLine(string.Join(" ", collection));
+        }
+    }
+}
