5f2ac01076febe2330d2720afa75213f1721880c
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-03-11 19:08:08 +0200

    Text processing lab
    3rd problem is solved

---
+++ C# Fundamentals/TextProcessingLab/3.Substring/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _3.Substring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string stringForRemove = Console.ReadLine();
+
+            string word = Console.ReadLine();
+
+            while (word.Contains(stringForRemove.ToLower()))
+            {
+                word = word.Replace(stringForRemove.ToLower(), "");
+            }
+
+            Console.WriteLine(word);
+        }
+    }
+}
