1f5951808eab674bd155c1bb017948b565076a96
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-03-11 19:00:08 +0200

    Text processing
    Lab start solving the problems

---
+++ C# Fundamentals/TextProcessingLab/1.ReverseStrings/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _1.ReverseStrings
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> collection = new Dictionary<string, string>();
+
+            string word;
+            while ((word = Console.ReadLine()) != "end")
+            {
+                string reversedWord = string.Join("", word.Reverse());
+
+                collection.Add(word, reversedWord);
+            }
+
+            string result = GetResult(ref collection);
+            Console.WriteLine(result);
+        }
+
+        private static string GetResult(ref Dictionary<string, string> collection)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var word in collection)
+            {
+                sb.AppendLine($"{word.Key} = {word.Value}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
