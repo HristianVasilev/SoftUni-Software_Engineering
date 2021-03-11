51e78e5c543551054506346fc6e2b8116481d378
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-03-11 19:03:31 +0200

    Text processing
    2nd problem is successfully solved!

---
+++ C# Fundamentals/TextProcessingLab/2.RepeatStrings/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Text;
+
+namespace _2.RepeatStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] array = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var item in array)
+            {
+                for (int i = 0; i < item.Length; i++)
+                {
+                    sb.Append(item);
+                }
+            }
+
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
