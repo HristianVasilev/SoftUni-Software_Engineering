87ab4aa1699784747b41391cfb2efa7d998eee86
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-03-12 08:05:01 +0200

    Text Processing Lab
    4th problem is solved!

---
+++ C# Fundamentals/TextProcessingLab/4.TextFilter/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+
+namespace _4.TextFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] remove = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            string text = Console.ReadLine();
+
+            foreach (var word in remove)
+            {
+               text= text.Replace(word, new string('*', word.Length));
+            }
+
+            Console.WriteLine(text);
+        }
+    }
+}
