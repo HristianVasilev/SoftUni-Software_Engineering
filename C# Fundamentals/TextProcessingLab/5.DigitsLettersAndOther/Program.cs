51ff917380f3f6d5f00ee48f9018c500e4588810
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-03-12 15:46:49 +0200

    Text processing lab
    5th problem solved

---
+++ C# Fundamentals/TextProcessingLab/5.DigitsLettersAndOther/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _5.DigitsLettersAndOther
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mandzha = Console.ReadLine();
+
+            string digits = string.Join("", mandzha.Where(c => char.IsDigit(c)));
+            string letters = string.Join("", mandzha.Where(c => char.IsLetter(c)));
+            string chars = string.Join("", mandzha.Where(c => !char.IsDigit(c) && !char.IsLetter(c)));
+
+            Console.WriteLine(digits);
+            Console.WriteLine(letters);
+            Console.WriteLine(chars);
+        }
+    }
+}
