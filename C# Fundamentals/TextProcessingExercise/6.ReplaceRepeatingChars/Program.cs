7cba4f35bef1914c67ea9fd70a6f17e2c5161a8d
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-03-14 11:22:53 +0200

    Text processing exercise
    6th problem is solved!

---
+++ C# Fundamentals/TextProcessingExercise/6.ReplaceRepeatingChars/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace _6.ReplaceRepeatingChars
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append(word[0]);
+
+            for (int i = 1; i < word.Length; i++)
+            {
+                if (word[i] == word[i - 1])
+                {
+                    continue;
+                }
+
+                sb.Append(word[i]);
+            }
+
+            Console.WriteLine(sb.ToString().TrimEnd());
+        }
+    }
+}
