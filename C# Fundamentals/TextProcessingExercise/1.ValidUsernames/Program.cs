14855b8180fe97eda0c6128013d8a75349067a9d
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-03-14 10:25:48 +0200

    Text processing exercise
    1st problem successfully solved!

---
+++ C# Fundamentals/TextProcessingExercise/1.ValidUsernames/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+
+namespace _1.ValidUsernames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] usernames = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            foreach (var username in usernames)
+            {
+                if (ValidUsername(username))
+                {
+                    Console.WriteLine(username);
+                }
+            }
+        }
+
+        private static bool ValidUsername(string username)
+        {
+            bool betweenRange = username.Length >= 3 && username.Length <= 16;
+
+            if (!betweenRange)
+            {
+                return false;
+            }
+
+            bool containsDifferentFromLettersAndDigits = username.Any(c => !char.IsLetterOrDigit(c));
+
+            if (containsDifferentFromLettersAndDigits)
+            {
+                char[] diff = username.Where(c => !char.IsLetterOrDigit(c)).ToArray();
+
+                if (diff.Any(c => c != '-' && c != '_'))
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
