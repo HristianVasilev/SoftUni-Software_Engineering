49205ff1e1898d19e4850607e19bfeb946095852
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-03-14 11:16:07 +0200

    Text processing
    5th problem - Big number division is solved without using "BigInteger" data type.

---
+++ C# Fundamentals/TextProcessingExercise/5.MultiplyBigNumber/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _5.MultiplyBigNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+            byte digit = byte.Parse(Console.ReadLine());
+
+            if (digit == 0)
+            {
+                Console.WriteLine(0);
+                return;
+            }
+
+            List<int> array = new List<int>();
+            NubmerToArray(number, array);
+
+            while (array[0] == 0)
+            {
+                array.RemoveAt(0);
+            }
+
+            int balance = 0;
+
+            List<int> product = new List<int>();
+
+
+            for (int i = array.Count - 1; i >= 0; i--)
+            {
+                product.Add(((array[i] * digit) + balance) % 10);
+
+                balance = (array[i] * digit + balance) / 10;
+            }
+
+            if (balance > 0)
+            {
+                product.Add(balance);
+            }
+            product.Reverse();
+            Console.WriteLine(String.Join("", product));
+
+        }
+
+        private static void NubmerToArray(string number, List<int> list)
+        {
+            for (int i = 0; i < number.Length; i++)
+            {
+                list.Add(int.Parse((number[i]).ToString()));
+            }
+        }
+    }
+}
