f99b0661de82b0db97aa66789cd2945805ab352f
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-03-14 12:19:32 +0200

    Text processing exercise
    7th problem is solved!

--- C# Fundamentals/TextProcessingExercise/7.StringExplosion/Program.cs
+++ C# Fundamentals/TextProcessingExercise/7.StringExplosion/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using System.Text;
 
-namespace _7.StringExplosion
+namespace _07.StringExplosion
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string input = Console.ReadLine();
+
+            StringBuilder sb = new StringBuilder();
+            int power = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char current = input[i];
+
+                if (current == '>')
+                {
+                    sb.Append(current);
+                    power += int.Parse(input[i + 1].ToString());
+                }
+                else if (power != 0)
+                {
+                    power--;
+                }
+                else
+                {
+                    sb.Append(current);
+                }
+            }
+            Console.WriteLine(sb);
         }
     }
 }
