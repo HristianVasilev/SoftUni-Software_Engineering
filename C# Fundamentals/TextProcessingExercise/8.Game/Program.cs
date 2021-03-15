114fa087121ed601c5b471526e92650a9cd12974
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-03-15 15:59:11 +0200

    Text processing exercise
    Alll problems solved!

---
+++ C# Fundamentals/TextProcessingExercise/8.Game/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace _08.LettersChangeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            double total = 0;
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                string current = input[i];
+
+                char first = current[0];
+                string n = string.Empty;
+                char last = current[current.Length - 1];
+
+                for (int j = 0; j < current.Length; j++)
+                {
+                    if (char.IsDigit(current[j]))
+                    {
+                        n += current[j];
+                    }
+                }
+
+                double number = int.Parse(n);
+
+                // firstSymbol
+                int letterPosition = 0;
+
+                if (char.IsUpper(first))
+                {
+                    // uppercase -> divide the number by letter position
+                    letterPosition = first - 64;
+                    number /= letterPosition;
+                }
+                else if (char.IsLower(first))
+                {
+                    letterPosition = first - 96;
+                    number *= letterPosition;
+                }
+
+                // lastSymbol
+
+                if (char.IsUpper(last))
+                {
+                    letterPosition = last - 64;
+                    number -= letterPosition;
+                }
+                else if (char.IsLower(last))
+                {
+                    letterPosition = last - 96;
+                    number += letterPosition;
+                }
+
+                total += number;
+            }
+
+            Console.WriteLine($"{total:f2}");
+
+        }
+    }
+}
