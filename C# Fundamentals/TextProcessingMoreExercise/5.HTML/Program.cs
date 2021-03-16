a2440bb6a8c18cbe3071aa8b67af6546cb65f60a
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-03-16 21:12:32 +0200

    Text processing more exercise
    5th problem, html solved!

---
+++ C# Fundamentals/TextProcessingMoreExercise/5.HTML/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Text;
+
+namespace _5.HTML
+{
+    class Program
+    {
+        private static StringBuilder html;
+
+        static void Main(string[] args)
+        {
+            html = new StringBuilder();
+
+            string title = Console.ReadLine();
+            string content = Console.ReadLine();
+
+            GenerateHtml(title, "h1");
+            GenerateHtml(content, "article");
+
+            string comment;
+            while ((comment = Console.ReadLine()) != "end of comments")
+            {
+                GenerateHtml(comment, "div");
+            }
+
+            string result = html.ToString().TrimEnd();
+            Console.WriteLine(result);
+        }
+
+        private static void GenerateHtml(string word, string tag)
+        {
+            html.AppendLine($"<{tag}>")
+                .AppendLine($"    {word}")
+                .AppendLine($"</{tag}>");        
+        }
+    }
+}
