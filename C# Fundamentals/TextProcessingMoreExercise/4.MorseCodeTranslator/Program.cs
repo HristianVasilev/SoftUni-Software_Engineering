fdf4f99f3b9239830cb7a0c9e9407995053db61b
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-03-16 20:46:27 +0200

    More exercise of text processing
    4th problem morse code is solved

---
+++ C# Fundamentals/TextProcessingMoreExercise/4.MorseCodeTranslator/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _4.MorseCodeTranslator
+{
+    class Program
+    {
+        private static Dictionary<string, string> morse;
+
+        static void Main(string[] args)
+        {
+            morse = new Dictionary<string, string>()
+            {
+                {"A",".-"},
+                {"B","-..."},
+                {"C","-.-."},
+                {"D","-.."},
+                {"E","."},
+                {"F","..-."},
+                {"G","--."},
+                {"H","...."},
+                {"I",".."},
+                {"J",".---"},
+                {"K","-.-"},
+                {"L",".-.."},
+                {"M","--"},
+                {"N","-."},
+                {"O","---"},
+                {"P",".--."},
+                {"Q","--.-"},
+                {"R",".-."},
+                {"S","..."},
+                {"T","-"},
+                {"U","..-"},
+                {"V","...-"},
+                {"W",".--"},
+                {"X","-..-"},
+                {"Y","-.--"},
+                {"Z","--.."},
+            };
+
+            string[] morseCodeTokens = Console.ReadLine().Split('|', StringSplitOptions.RemoveEmptyEntries);
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var word in morseCodeTokens)
+            {
+                string translation = Translate(word);
+                sb.Append(translation + ' ');
+            }
+
+            Console.WriteLine(sb.ToString().TrimEnd());
+        }
+
+        private static string Translate(string word)
+        {
+            string[] letters = word.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var letter in letters)
+            {
+                string translatedLetter = morse.First(x => x.Value == letter).Key;
+                sb.Append(translatedLetter);
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
