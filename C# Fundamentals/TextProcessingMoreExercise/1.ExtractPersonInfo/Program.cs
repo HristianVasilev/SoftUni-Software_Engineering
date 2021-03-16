5b41bc7ae4e5d73d3b7ef52b74bd4186ed7efc36
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-03-16 19:34:56 +0200

    Text processing More exercise
    1st problem solved

---
+++ C# Fundamentals/TextProcessingMoreExercise/1.ExtractPersonInfo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace _1.ExtractPersonInfo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte count = byte.Parse(Console.ReadLine());
+
+            for (int i = 0; i < count; i++)
+            {
+                string[] stringLine = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string name = stringLine
+                    .First(x => x.Any(c => c == '@') && x.Any(c => c == '|'))
+                    .Trim(new char[] { '@', '|' });
+                
+                string age = stringLine
+                    .First(x => x.Any(c => c == '#') && x.Any(c => c == '*'))
+                    .Trim(new char[] { '#', '*' });
+
+                Console.WriteLine($"{name} is {age} years old.");
+            }
+        }
+
+    }
+}
