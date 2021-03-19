ad0b80e0ffc10a269dd3af07ca2e8ef99318efb7
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-03-19 19:06:55 +0200

    Regex Lab
    Match phone number problem is finally solved!

---
+++ C# Fundamentals/RegularExpressionsLab/2.MatchPhoneNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _2.MatchPhoneNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string pattern = @"\+359(?<separator>(?: |-))2\1\d{3}\1\d{4}(?!\d)";
+            Regex regex = new Regex(pattern);
+
+            MatchCollection matches = regex.Matches(input);
+
+            string result = string.Join(", ", matches);
+            Console.WriteLine(result);
+        }
+    }
+}
