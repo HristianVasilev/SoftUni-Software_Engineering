af0abd80aa44e67101702f8ae8a49801adb755d8
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-03-19 20:23:48 +0200

    Regex exercise
    2nd problem - Race is successfully solved!

--- C# Fundamentals/RegexExercise/2.Race/Program.cs
+++ C# Fundamentals/RegexExercise/2.Race/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
 
 namespace _2.Race
 {
@@ -6,7 +10,60 @@ namespace _2.Race
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Dictionary<string, int> raceResults = new Dictionary<string, int>();
+
+            string[] participants = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            string input;
+            while ((input = Console.ReadLine()) != "end of race")
+            {
+                string pattern = @"(?<name>[A-Za-z])";
+                MatchCollection nameChars = Regex.Matches(input, pattern);
+
+                string name = string.Join(string.Empty, nameChars.Select(x => x.Value));
+
+                if (!participants.Contains(name))
+                {
+                    continue;
+                }
+
+                pattern = @"(?<distance>\d)";
+                MatchCollection distanceChars = Regex.Matches(input, pattern);
+
+                int distance = distanceChars.Select(x => int.Parse(x.Value)).Sum();
+
+                AddPlayer(name, distance, ref raceResults);
+            }
+
+            string result = GetTop3Racers(raceResults);
+            Console.WriteLine(result);
+        }
+
+        private static string GetTop3Racers(Dictionary<string, int> raceResults)
+        {
+            string[] orderedRacers = raceResults
+                .OrderByDescending(x => x.Value)
+                .Take(3)
+                .Select(x => x.Key)
+                .ToArray();
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"1st place: {orderedRacers[0]}")
+                .AppendLine($"2nd place: {orderedRacers[1]}")
+                .AppendLine($"3rd place: {orderedRacers[2]}");
+
+            return sb.ToString().TrimEnd();
+        }
+
+        private static void AddPlayer(string name, int distance, ref Dictionary<string, int> raceResults)
+        {
+            if (!raceResults.ContainsKey(name))
+            {
+                raceResults.Add(name, 0);
+            }
+
+            raceResults[name] += distance;
         }
     }
 }
