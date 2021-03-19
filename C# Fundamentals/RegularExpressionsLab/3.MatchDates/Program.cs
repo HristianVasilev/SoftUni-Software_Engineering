3370052cb072ff16f6008c39a63e05331eb268cd
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-03-19 19:26:30 +0200

    Regex lab
    Match dates problem is finally solved! I hate regexes!

---
+++ C# Fundamentals/RegularExpressionsLab/3.MatchDates/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _3.MatchDates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string pattern = @"(?<=^|\s)(?<day>\d{2})(?<separator>\/|\-|\.)(?<month>[A-Z][a-z]{2})\2(?<year>\d{4})(?!\d)";
+            Regex regex = new Regex(pattern);
+
+            MatchCollection collection = regex.Matches(input);
+
+            foreach (Match date in collection)
+            {
+                string day = date.Groups["day"].Value;
+                string month = date.Groups["month"].Value;
+                string year = date.Groups["year"].Value;
+
+                Console.WriteLine($"Day: {day}, Month: {month}, Year: {year}");
+            }
+
+        }
+    }
+}
