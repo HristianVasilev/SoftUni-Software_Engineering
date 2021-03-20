67cc47225942057c6d3c5022df7021381d9115b3
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-03-20 09:02:14 +0200

    Regex exercise
    3rd problem is solved!

---
+++ C# Fundamentals/RegexExercise/3.SoftUniBarIncome/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _3.SoftUniBarIncome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal totalIncome = 0;
+
+            string input;
+            while ((input = Console.ReadLine()) != "end of shift")
+            {
+                string pattern = @"[%](?<customer>[A-Z][a-z]+)[%]([^.|$%]+(?<!\d))?[<](?<product>\w+)[>]([^.|$%]+)?[|](?<count>\d+)[|]([^.|$%]+(?<!\d))?(?<price>(?:\d+|\d+.\d+))[$]";
+                Regex regex = new Regex(pattern);
+
+                Match match = regex.Match(input);
+
+                if (!match.Success)
+                {
+                    continue;
+                }
+
+                string customerName = match.Groups["customer"].Value;
+                string product = match.Groups["product"].Value;
+                int count = int.Parse(match.Groups["count"].Value);
+                decimal price = decimal.Parse(match.Groups["price"].Value);
+                decimal totalPrice = price * count*1.0m;
+
+                totalIncome += totalPrice;
+
+                Console.WriteLine($"{customerName}: {product} - {totalPrice:f2}");
+            }
+
+            Console.WriteLine($"Total income: {totalIncome:f2}");
+        }
+    }
+}
