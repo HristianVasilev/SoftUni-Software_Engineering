cfb5993f2d76b683d47d7d7e2a59983e745ebf88
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-03-21 10:50:28 +0200

    Regular expressions
    All exercises are completed successfully!

---
+++ C# Fundamentals/RegexExercise/6.ExtactEmails/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _6.ExtactEmails
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            string pattern = @"(?<user>(?<=\s)[a-z0-9]+([._-]?[a-z0-9]+)*(?=@))@(?<host>(?<=@)[a-z]+([-]?[a-z]+)*?(\.[a-z]+)+)";
+            Regex regex = new Regex(pattern);
+            MatchCollection matches = regex.Matches(input);
+
+            foreach (Match match in matches)
+            {
+                Console.WriteLine(match.Value);
+            }
+        }
+    }
+}
