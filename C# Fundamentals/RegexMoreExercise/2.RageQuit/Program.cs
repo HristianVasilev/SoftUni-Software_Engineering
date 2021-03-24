a5a3246dc34c459b7543f7a482d30287a10ea0e5
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-03-24 07:54:29 +0200

    Regex
    2nd problem from more exercise is successfully solved!

---
+++ C# Fundamentals/RegexMoreExercise/2.RageQuit/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace _2.RageQuit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            string input = Console.ReadLine();
+
+            string pattern = @"(?<symbols>\D+)(?<count>\d+)";
+
+            MatchCollection collection = Regex.Matches(input, pattern);
+
+            foreach (Match match in collection)
+            {
+                string symbols = match.Groups["symbols"].Value.ToUpper();
+                int count = int.Parse(match.Groups["count"].Value);
+
+                Replicate(ref sb, symbols, count);
+            }
+
+            string result = sb.ToString().TrimEnd();
+            int uniqueSymbols = result.ToCharArray().Distinct().Count();
+
+            Console.WriteLine($"Unique symbols used: {uniqueSymbols}");
+            Console.WriteLine(result);
+        }
+
+        private static void Replicate(ref StringBuilder sb, string symbols, int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                sb.Append(symbols);
+            }
+        }
+
+    }
+}
