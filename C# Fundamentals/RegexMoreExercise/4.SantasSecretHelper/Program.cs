dde456fc7a72045ed0a2e96ce6f27c8f4838bec8
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-03-25 15:01:08 +0200

    Regex more exericises
    All complete!

---
+++ C# Fundamentals/RegexMoreExercise/4.SantasSecretHelper/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace _4.SantasSecretHelper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> childs = new List<string>();
+
+            int key = int.Parse(Console.ReadLine());
+
+            string input;
+            while ((input = Console.ReadLine()) != "end")
+            {
+                string message = Decrypt(input, key);
+
+                string pattern = @"(@(?<name>[A-Za-z]+)[^@!:>-]*!(?<behaviour>[GN])!)";
+                Match match = Regex.Match(message, pattern);
+
+                string name = match.Groups["name"].Value;
+                string behaviour = match.Groups["behaviour"].Value;
+
+                if (behaviour == "G")
+                {
+                    childs.Add(name);
+                }
+            }
+
+            string result = string.Join(Environment.NewLine, childs);
+            Console.WriteLine(result);
+        }
+
+        private static string Decrypt(string input, int key)
+        {
+            var decripted = input.Select(x => (char)(x - key));
+            return string.Join(string.Empty, decripted);
+        }
+    }
+}
