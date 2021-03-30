bca8e3c601d08791fa5103756723c1920517a69a
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-03-30 18:13:54 +0300

    Exam preparation
    2nd problem is not solved successfully. I get only 20 points from 100. I think that there is a problem with the condition. On the other hand, 3rd problem is successfully solved perfect at once.

---
+++ C# Fundamentals/Exams/FinalExamPreparation/FinalExam09.08.2020/Problem2.DestinationMapper/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+namespace Problem2.DestinationMapper
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            string pattern = @"(=|\/)([A-Z][a-z]{2,})\1";
+
+            var matches = Regex.Matches(input, pattern);
+
+            string[] destinations = matches.Select(x => x.Groups[2].Value).ToArray();
+            int travelPoints = destinations.Sum(x => x.Length);
+
+            Console.WriteLine($"Destinations: {string.Join(", ", destinations)}");
+            Console.WriteLine($"Travel Points: {travelPoints}");
+        }
+    }
+}
---
+++ C# Fundamentals/Exams/FinalExamPreparation/FinalExam09.08.2020/Problem3.PlantDiscovery/Program.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Problem3.PlantDiscovery
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Plant> plantsInfo = new Dictionary<string, Plant>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] tokens = Console.ReadLine().Split("<->", StringSplitOptions.RemoveEmptyEntries);
+
+                RegisterPlant(tokens, ref plantsInfo);
+            }
+
+            string command;
+            while ((command = Console.ReadLine()) != "Exhibition")
+            {
+                string[] tokens = command.Split(":");
+                string action = tokens[0];
+
+                string info = tokens[1].Trim();
+
+                switch (action)
+                {
+                    case "Rate":
+                        Rate(info, ref plantsInfo);
+                        break;
+                    case "Update":
+                        Update(info, ref plantsInfo);
+                        break;
+                    case "Reset":
+                        Reset(info, ref plantsInfo);
+                        break;
+
+                    default:
+                        throw new NotImplementedException("Invalid command!");
+                }
+            }
+
+            PrintResult(ref plantsInfo);
+
+        }
+
+        private static void PrintResult(ref Dictionary<string, Plant> plantsInfo)
+        {
+            Console.WriteLine("Plants for the exhibition:");
+
+            Func<KeyValuePair<string, Plant>, double> ordering = x =>
+            {
+                if (x.Value.Rating.Count == 0)
+                {
+                    return 0;
+                }
+
+                return x.Value.Rating.Average();
+            };
+
+            var collection = plantsInfo.OrderByDescending(x => x.Value.Rarity)
+                .ThenByDescending(ordering)
+                .ToDictionary(k => k.Key, v => v.Value);
+
+            foreach (var plant in collection)
+            {
+                double rating;
+
+                if (plant.Value.Rating.Count == 0)
+                {
+                    rating = 0;
+                }
+                else
+                {
+                    rating = plant.Value.Rating.Average();
+                }
+
+                Console.WriteLine($"- {plant.Key}; Rarity: {plant.Value.Rarity}; Rating: {rating:f2}");
+            }
+        }
+
+
+
+        private static void Reset(string plant, ref Dictionary<string, Plant> plantsInfo)
+        {
+            if (!plantsInfo.ContainsKey(plant))
+            {
+                Console.WriteLine("error");
+                return;
+            }
+
+            plantsInfo[plant].Rating.Clear();
+        }
+
+        private static void Update(string info, ref Dictionary<string, Plant> plantsInfo)
+        {
+            string[] tokens = info.Split(" - ", StringSplitOptions.RemoveEmptyEntries);
+
+            string plant = tokens[0];
+            int newRarity = int.Parse(tokens[1]);
+
+            if (!plantsInfo.ContainsKey(plant))
+            {
+                Console.WriteLine("error");
+                return;
+            }
+
+            plantsInfo[plant].Rarity = newRarity;
+        }
+
+        private static void Rate(string info, ref Dictionary<string, Plant> plantsInfo)
+        {
+            string[] tokens = info.Split(" - ", StringSplitOptions.RemoveEmptyEntries);
+
+            string plant = tokens[0];
+
+            if (!plantsInfo.ContainsKey(plant))
+            {
+                Console.WriteLine("error");
+                return;
+            }
+
+            double rating = double.Parse(tokens[1]);
+
+            plantsInfo[plant].Rating.Add(rating);
+        }
+
+
+
+        private static void RegisterPlant(string[] tokens, ref Dictionary<string, Plant> plantsInfo)
+        {
+            string plant = tokens[0];
+            int rarity = int.Parse(tokens[1]);
+
+            if (!plantsInfo.ContainsKey(plant))
+            {
+                plantsInfo.Add(plant, new Plant());
+            }
+
+            plantsInfo[plant].Rarity = rarity;
+        }
+    }
+
+    class Plant
+    {
+        public Plant()
+        {
+            this.Rating = new List<double>();
+        }
+
+        public int Rarity { get; set; }
+        public List<double> Rating { get; set; }
+    }
+
+}
