8e013ac175df3369455a3d5d8e79b2ff673086e0
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-03-30 16:50:45 +0300

    Exam preparation
    1st problem solved!

---
+++ C# Fundamentals/Exams/FinalExamPreparation/FinalExam09.08.2020/Problem1.WorldTour/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace Problem1.WorldTour
+{
+    class Program
+    {
+        private static string text;
+
+        static void Main(string[] args)
+        {
+            text = Console.ReadLine();
+
+            string command;
+            while ((command = Console.ReadLine()) != "Travel")
+            {
+                string[] tokens = command.Split(':', StringSplitOptions.RemoveEmptyEntries);
+
+                switch (tokens[0])
+                {
+                    case "Add Stop":
+                        AddStop(tokens);
+                        break;
+                    case "Remove Stop":
+                        RemoveStop(tokens);
+                        break;
+                    case "Switch":
+                        Switch(tokens);
+                        break;
+                    default:
+                        throw new NotImplementedException("Invalid command!");
+                }
+
+                Console.WriteLine(text);
+            }
+
+            Console.WriteLine($"Ready for world tour! Planned stops: {text}");
+        }
+
+        private static void Switch(string[] tokens)
+        {
+            string oldString = tokens[1];
+            string newString = tokens[2];
+
+            if (text.Contains(oldString))
+            {
+                text = text.Replace(oldString, newString);
+            }
+        }
+
+        private static void RemoveStop(string[] tokens)
+        {
+            byte startIndex = byte.Parse(tokens[1]);
+            byte endIndex = byte.Parse(tokens[2]);
+
+            if (ValidIndex(startIndex) && ValidIndex(endIndex))
+            {
+                byte count = (byte)(endIndex - startIndex + 1);
+                text = text.Remove(startIndex, count);
+            }
+        }
+
+        private static void AddStop(string[] tokens)
+        {
+            byte index = byte.Parse(tokens[1]);
+
+            if (ValidIndex(index))
+            {
+                string newString = tokens[2];
+
+                text = text.Insert(index, newString);
+            }
+        }
+
+        private static bool ValidIndex(byte index)
+        {
+            return index >= 0 && index < text.Length;
+        }
+    }
+}
