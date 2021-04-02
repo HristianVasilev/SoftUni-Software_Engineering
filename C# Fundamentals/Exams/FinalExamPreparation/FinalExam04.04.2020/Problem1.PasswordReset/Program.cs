40b6b92d234e373086c40256966cacb9d9c9e41f
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-04-02 19:27:18 +0300

    Start exam preparation 4

---
+++ C# Fundamentals/Exams/FinalExamPreparation/FinalExam04.04.2020/Problem1.PasswordReset/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Problem1.PasswordReset
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
