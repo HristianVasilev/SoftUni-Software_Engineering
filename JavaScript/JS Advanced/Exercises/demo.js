492265c1c18aee9c1332e4659529b3cc814f464e
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-04-07 15:45:02 +0300

    First steps in Java Script :)

---
+++ JavaScript/JS Advanced/Exercises/demo.js
@@ -0,0 +1,12 @@
+let stringVar = 'some text';
+let stringVar2 = "Some text";
+
+let numberVar = 10;
+let nubmerVar2 = 3.14;
+
+let boolean = true;
+let boolean2 = false;
+
+let someVariable = undefined;
+
+console.log(someVariable)
