a52e71878c6942aa806fbc354d2f99c8e5040041
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-05-23 15:14:49 +0300

    Stacks and queues problem
    Print even numbers problem solved!

---
+++ C#/C# Advanced/StacksAndQueues.Lab/05.PrintEvenNumbers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _05.PrintEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> queue = new Queue<int>(input);
+
+            string result = Filter(queue);
+            Console.WriteLine(result);
+        }
+
+        private static string Filter(Queue<int> queue)
+        {
+            List<int> filteredNumbers = new List<int>();
+
+            while (queue.Count > 0)
+            {
+                int currentNumber = queue.Dequeue();
+
+                if (currentNumber %2 ==0)
+                {
+                    filteredNumbers.Add(currentNumber);
+                }
+            }
+
+            return string.Join(", ", filteredNumbers);
+        }
+    }
+}
