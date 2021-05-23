4b8d4ef52eda80d6b95ac734399f88a5c2cabe0b
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-05-23 13:21:26 +0300

    StacksAndQueues
    Simple calculator is successfully created.

---
+++ C#/C# Advanced/StacksAndQueues.Lab/03.SimpleCalculator/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.SimpleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<string> input = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            Stack<string> stack = new Stack<string>(input);
+            int result = 0;
+
+            while (stack.Count > 0)
+            {
+                string current = stack.Pop();
+
+                if (int.TryParse(current, out int number))
+                {
+                    if (stack.Count > 0)
+                    {
+                        string sign = stack.Pop();
+
+                        if (sign == "-")
+                        {
+                            number *= -1;
+                        }
+                    }
+                }
+                else
+                {
+                    throw new InvalidCastException();
+                }
+
+                result += number;
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
