b218e5dcc806fb5844fc95e8b707403dc646fb29
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-05-24 08:26:51 +0300

    Stacks and queues
    Supermarket problem is solved.

---
+++ C#/C# Advanced/StacksAndQueues.Lab/06.Supermarket/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.Supermarket
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> queue = new Queue<string>();
+
+            string input;
+            while ((input = Console.ReadLine()) != "End")
+            {
+                if (input == "Paid")
+                {
+                    Console.WriteLine(string.Join(Environment.NewLine, queue));
+                    queue.Clear();
+                    continue;
+                }
+
+                queue.Enqueue(input);
+            }
+
+            Console.WriteLine($"{queue.Count} people remaining.");
+        }
+    }
+}
