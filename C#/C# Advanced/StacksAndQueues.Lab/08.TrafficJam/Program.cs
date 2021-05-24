e5ad1420bc91aa76ebf129205fa7e8ea5cbdd8e5
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-05-24 08:28:53 +0300

    Stacks and queues
    Traffic jam problem is solved and the lab is done!

---
+++ C#/C# Advanced/StacksAndQueues.Lab/08.TrafficJam/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _08.TrafficJam
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Queue<string> queue = new Queue<string>();
+            int passed = 0;
+
+            string command;
+            while ((command = Console.ReadLine()) != "end")
+            {
+                if (command == "green")
+                {
+                    PassCars(n, ref queue, ref passed);
+                    continue;
+                }
+
+                queue.Enqueue(command);
+            }
+
+            Console.WriteLine($"{passed} cars passed the crossroads.");
+        }
+
+        private static void PassCars(int n, ref Queue<string> queue, ref int passed)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                if (queue.Count == 0)
+                {
+                    return;
+                }
+
+                string car = queue.Dequeue();
+                Console.WriteLine($"{car} passed!");
+                passed++;
+            }
+        }
+    }
+}
