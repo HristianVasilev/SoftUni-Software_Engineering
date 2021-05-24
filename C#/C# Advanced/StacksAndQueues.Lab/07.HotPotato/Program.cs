fd8273a74873446152a118aaddf4895939af1d8f
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-05-24 08:27:32 +0300

    Stacks and queues
    Hot potato problem is not 100 completed!

---
+++ C#/C# Advanced/StacksAndQueues.Lab/07.HotPotato/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07.HotPotato
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int tosses = int.Parse(Console.ReadLine());
+
+            Queue<string> kids = new Queue<string>(input.Split(' ', StringSplitOptions.RemoveEmptyEntries));
+
+            while (kids.Count > 1)
+            {
+                for (int i = 0; i < tosses - 1; i++)
+                {
+                    string kid = kids.Dequeue();
+                    kids.Enqueue(kid);
+                }
+                Console.WriteLine("Removed " + kids.Dequeue());
+            }
+
+            Console.WriteLine("Last is " + kids.Dequeue());
+        }
+    }
+}
