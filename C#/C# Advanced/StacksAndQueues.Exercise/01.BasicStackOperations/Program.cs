fb7af6b04cf09849ebfb847a7a3788896d3aa9a2
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-05-25 21:55:58 +0300

    Stacks and queue
    BAsic stack operations task is solved

---
+++ C#/C# Advanced/StacksAndQueues.Exercise/01.BasicStackOperations/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.BasicStackOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arguments = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int toPush = arguments[0];
+            int toPop = arguments[1];
+            int toLook = arguments[2];
+
+            Stack<int> stack = new Stack<int>();
+
+            PushStack(toPush, ref stack);
+            PopStack(toPop, ref stack);
+            bool contains = LookupStack(toLook, ref stack);
+
+            string output = GetOutput(stack, contains);
+            Console.WriteLine(output);
+        }
+
+        private static string GetOutput(Stack<int> stack, bool contains)
+        {
+            if (contains)
+            {
+                return "true";
+            }
+            else if (!contains && stack.Count > 0)
+            {
+                return $"{stack.Min()}";
+            }
+            else
+            {
+                return $"{0}";
+            }
+        }
+
+        private static bool LookupStack(int toLook, ref Stack<int> stack)
+        {
+            if (stack.Contains(toLook))
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        private static void PopStack(int toPop, ref Stack<int> stack)
+        {
+            for (int i = 0; i < toPop; i++)
+            {
+                stack.Pop();
+            }
+        }
+
+        private static void PushStack(int toPush, ref Stack<int> stack)
+        {
+            int[] integers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            for (int i = 0; i < toPush; i++)
+            {
+                stack.Push(integers[i]);
+            }
+        }
+    }
+}
