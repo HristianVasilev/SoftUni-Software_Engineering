35c5eab8f5ab496448e2ecd5f21eeb96a558d604
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-05-25 21:56:43 +0300

    Stacks and queues
    Basic queue operations task is solved!

---
+++ C#/C# Advanced/StacksAndQueues.Exercise/02.BasicQueueOperations/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.BasicQueueOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arguments = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int toEnqueue = arguments[0];
+            int toDequeue = arguments[1];
+            int toLook = arguments[2];
+
+            Queue<int> queue = new Queue<int>(Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray());
+
+            Dequeue(toDequeue, ref queue);
+            string result = Look(toLook, ref queue);
+
+            Console.WriteLine(result);
+        }
+
+        private static string Look(int toLook, ref Queue<int> queue)
+        {
+            if (queue.Count == 0)
+            {
+                return "0";
+            }
+
+            if (queue.Contains(toLook))
+            {
+                return "true";
+            }
+
+            return $"{queue.Min()}";
+        }
+
+        private static void Dequeue(int toDequeue, ref Queue<int> queue)
+        {
+            for (int i = 0; i < toDequeue; i++)
+            {
+                queue.Dequeue();
+            }
+        }
+    }
+}
