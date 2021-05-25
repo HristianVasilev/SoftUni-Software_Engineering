9a6ca669a988dcadb3a931b75ec4975d6b374897
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-05-25 21:57:25 +0300

    Stacks and queues
    Max and min element at the type of array. Problem is solved!

---
+++ C#/C# Advanced/StacksAndQueues.Exercise/03.MaximumAndMinimumElement/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.MaximumAndMinimumElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countOfQueries = int.Parse(Console.ReadLine());
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < countOfQueries; i++)
+            {
+                int[] tokens = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                int action = tokens[0];
+
+                switch (action)
+                {
+                    case 1:
+                        stack.Push(tokens[1]);
+                        break;
+                    case 2:
+                        DeleteTopOfStack(ref stack);
+                        break;
+                    case 3:
+                        MaxOfStack(ref stack);
+                        break;
+                    case 4:
+                        MinOfStack(ref stack);
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            string outputStack = string.Join(", ", stack);
+            Console.WriteLine(outputStack);
+        }
+
+        private static void MinOfStack(ref Stack<int> stack)
+        {
+            if (stack.Count == 0)
+            {
+                return;
+            }
+
+            Console.WriteLine(stack.Min());
+        }
+
+        private static void MaxOfStack(ref Stack<int> stack)
+        {
+            if (stack.Count == 0)
+            {
+                return;
+            }
+
+            Console.WriteLine(stack.Max());
+        }
+
+        private static void DeleteTopOfStack(ref Stack<int> stack)
+        {
+            if (stack.Count > 0)
+            {
+                stack.Pop();
+            }
+        }
+    }
+}
