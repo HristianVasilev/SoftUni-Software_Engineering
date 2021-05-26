ed1bf834cbddf09330b770b3443bdadb02db34c4
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-05-26 20:20:45 +0300

    Stacks and queues
    Fashion boutique is solved.

---
+++ C#/C# Advanced/StacksAndQueues.Exercise/05.FashionBoutique/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.FashionBoutique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> clothes = new Stack<int>(Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse));
+
+            int capacity = int.Parse(Console.ReadLine());
+
+            byte countOfRacks = GetCountOfRacks(clothes, capacity);
+            Console.WriteLine(countOfRacks);
+        }
+
+        private static byte GetCountOfRacks(Stack<int> clothes, int capacity)
+        {
+            byte racks = 1;
+            int currentRack = 0;
+
+            while (clothes.Count > 0)
+            {
+                if (currentRack + clothes.Peek() <= capacity)
+                {
+                    currentRack += clothes.Pop();
+                    continue;
+                }
+
+                racks++;
+                currentRack = 0;
+            }
+
+            return racks;
+        }
+    }
+}
