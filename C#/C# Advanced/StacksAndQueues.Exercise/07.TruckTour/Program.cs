12ad434febaf7e49dff689a9cd382583cf0180fa
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-05-26 20:22:58 +0300

    Stacks and queues
    Truck tour exercise is 100% completed.

---
+++ C#/C# Advanced/StacksAndQueues.Exercise/07.TruckTour/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _07.TruckTour
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int petrolPumps = int.Parse(Console.ReadLine());
+
+            
+
+
+        }
+    }
+}
