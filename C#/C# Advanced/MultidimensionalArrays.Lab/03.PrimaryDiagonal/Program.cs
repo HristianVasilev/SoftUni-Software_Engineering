49ea437b7f5fd08ec0a61998eae0ab0b80afa365
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-05-29 12:04:34 +0300

    Multidimensional arrays - lab
    Symbol matrix and primary diagonal

---
+++ C#/C# Advanced/MultidimensionalArrays.Lab/03.PrimaryDiagonal/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.PrimaryDiagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[size, size];
+            ReadMatrix(ref matrix);
+
+            string result = GetSumOfPrimaryDiagonal(matrix);
+            Console.WriteLine(result);
+        }
+
+        private static string GetSumOfPrimaryDiagonal(int[,] matrix)
+        {
+            int sum = 0;
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                sum += matrix[i, i];
+            }
+
+            return $"{sum}";
+        }
+
+        private static void ReadMatrix(ref int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                int[] currentRow = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = currentRow[j];
+                }
+            }
+        }
+    }
+}
---
+++ C#/C# Advanced/MultidimensionalArrays.Lab/04.SymbolInMatrix/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.SymbolInMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            char[,] matrix = new char[size, size];
+            ReadMatrix(ref matrix);
+
+            char symbol = char.Parse(Console.ReadLine());
+            string result = FindSymbol(symbol, matrix);
+            Console.WriteLine(result);
+        }
+
+        private static string FindSymbol(char symbol, char[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (matrix[i, j] == symbol)
+                    {
+                        return $"({i}, {j})";
+                    }
+                }
+            }
+
+            return $"{symbol} does not occur in the matrix ";
+        }
+
+        private static void ReadMatrix(ref char[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                string currentRow = Console.ReadLine();
+
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = currentRow[j];
+                }
+            }
+        }
+
+    }
+}
