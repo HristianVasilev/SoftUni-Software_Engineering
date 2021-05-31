dd80862bcd070e0532a34c5251235166aca55b1a
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-05-31 22:19:28 +0300

    SetsAndDictionariesAdvanced.Lab
    1st task - Count the same values in the array - Solved!
    2nd task = Average student grades - Solved!

---
+++ C#/C# Advanced/SetsAndDictionariesAdvanced.Lab/01.CountSameValuesInArray/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _01.CountSameValuesInArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+
+            Dictionary<double, int> data = new Dictionary<double, int>();
+
+            foreach (var number in numbers)
+            {
+                if (!data.ContainsKey(number))
+                {
+                    data.Add(number, 0);
+                }
+
+                data[number]++;
+            }
+
+            string output = GetOutput(data);
+            Console.WriteLine(output);
+        }
+
+        private static string GetOutput(Dictionary<double, int> data)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var item in data)
+            {
+                sb.AppendLine($"{item.Key} - {item.Value} times");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C#/C# Advanced/SetsAndDictionariesAdvanced.Lab/02.AverageStudentGrades/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _02.AverageStudentGrades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<decimal>> students = new Dictionary<string, List<decimal>>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] tokens = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string name = tokens[0];
+                decimal grade = decimal.Parse(tokens[1]);
+
+                GradeQuery(name, grade, ref students);
+            }
+
+            string result = GetResult(students);
+            Console.WriteLine(result);
+        }
+
+        private static string GetResult(Dictionary<string, List<decimal>> students)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            foreach (var student in students)
+            {
+                sb.AppendLine($"{student.Key} -> {string.Join(' ', student.Value.Select(x => x.ToString("F2")))} (avg: {student.Value.Average():f2})");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        private static void GradeQuery(string name, decimal grade, ref Dictionary<string, List<decimal>> students)
+        {
+            if (!students.ContainsKey(name))
+            {
+                students.Add(name, new List<decimal>());
+            }
+
+            students[name].Add(grade);
+        }
+    }
+}
