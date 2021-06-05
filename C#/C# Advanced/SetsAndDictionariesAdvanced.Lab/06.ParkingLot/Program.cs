d5dee99961dbf5b9bb14d1ba4a9235259a230033
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-06-05 08:48:38 +0300

    Sets and dictionaries lab
    All tasks are successfully completed.

---
+++ C#/C# Advanced/SetsAndDictionariesAdvanced.Lab/06.ParkingLot/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.ParkingLot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> parkingLot = new HashSet<string>();
+
+            string input;
+            while ((input = Console.ReadLine()) != "END")
+            {
+                string[] tokens = input.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+                string direction = tokens[0];
+                string carNumber = tokens[1];
+
+                switch (direction)
+                {
+                    case "IN":
+                        parkingLot.Add(carNumber);
+                        break;
+                    case "OUT":
+                        parkingLot.Remove(carNumber);
+                        break;
+                    default:
+                        throw new InvalidOperationException("Invalid direction!");
+                }
+            }
+
+            string result = GetResult(parkingLot);
+            Console.WriteLine(result);
+        }
+
+        private static string GetResult(HashSet<string> parkingLot)
+        {
+            if (parkingLot.Count == 0)
+            {
+                return "Parking Lot is Empty";
+            }
+
+            return string.Join(Environment.NewLine, parkingLot);
+        }
+    }
+}
---
+++ C#/C# Advanced/SetsAndDictionariesAdvanced.Lab/07.SoftUniParty/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _07.SoftUniParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> reservations = new List<string>();
+
+            bool add = true;
+
+            string input;
+            while (true)
+            {
+                input = Console.ReadLine();
+
+                if (input == "PARTY")
+                {
+                    add = false;
+                    continue;
+                }
+
+                if (input == "END")
+                {
+                    break;
+                }
+
+                if (add)
+                {
+                    reservations.Add(input);
+                }
+                else
+                {
+                    reservations.Remove(input);
+                }
+            }
+
+            string result = GetResult(reservations);
+            Console.WriteLine(result);
+        }
+
+        private static string GetResult(List<string> reservations)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"{reservations.Count}");
+
+            if (reservations.Count == 0)
+            {
+                return sb.ToString().TrimEnd();
+            }
+
+            List<string> vipGuests = new List<string>();
+            List<string> guests = new List<string>();
+
+            foreach (var guestNo in reservations)
+            {
+                if (char.IsDigit(guestNo[0]))
+                {
+                    vipGuests.Add(guestNo);
+                }
+                else
+                {
+                    guests.Add(guestNo);
+                }
+            }
+
+            if (vipGuests.Count > 0)
+            {
+                foreach (var vipGuest in vipGuests)
+                {
+                    sb.AppendLine(vipGuest);
+                }
+            }
+
+            foreach (var guest in guests)
+            {
+                sb.AppendLine(guest);
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
