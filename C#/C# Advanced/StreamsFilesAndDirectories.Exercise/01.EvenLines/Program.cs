424586b8f34c4c8f51b17210e0145baeb1183365
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-06-09 21:02:00 +0300

    Streams files and directories - exercise
    1. Even lines
    2. Line numbers

---
+++ C#/C# Advanced/StreamsFilesAndDirectories.Exercise/01.EvenLines/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _01.EvenLines
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using (StreamReader sr = new StreamReader("text.txt"))
+            {
+                byte counter = 0;
+                string line;
+                while ((line = await sr.ReadLineAsync()) != null)
+                {
+                    if (counter++ % 2 != 0)
+                    {
+                        continue;
+                    }
+
+                    char[] symbolsToReplace = new char[] { '-', ',', '.', '!', '?' };
+                    line = ReplaceSymbols(line, symbolsToReplace);
+
+                    string[] currentLineArray = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+                    Array.Reverse(currentLineArray);
+                    Console.WriteLine(string.Join(' ', currentLineArray));
+                }
+            }
+        }
+
+        private static string ReplaceSymbols(string line, char[] symbols)
+        {
+            foreach (var symbol in symbols)
+            {
+                line = line.Replace(symbol, '@');
+            }
+
+            return line;
+        }
+    }
+}
---
+++ C#/C# Advanced/StreamsFilesAndDirectories.Exercise/02.LineNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace _02.LineNumbers
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            using (StreamReader sr = new StreamReader("text.txt"))
+            {
+                using (StreamWriter sw = new StreamWriter("output.txt"))
+                {
+                    int counter = 1;
+                    string line;
+
+                    while ((line = await sr.ReadLineAsync()) != null)
+                    {
+                        int countOfLetters = line.ToCharArray().Where(x => char.IsLetter(x)).Count();
+                        int countOfPuncMarks = line.ToCharArray().Where(x => char.IsPunctuation(x)).Count();
+
+                        await sw.WriteLineAsync($"Line {counter++}: {line} ({countOfLetters})({countOfPuncMarks})");
+                    }
+                }
+            }
+        }
+    }
+}
