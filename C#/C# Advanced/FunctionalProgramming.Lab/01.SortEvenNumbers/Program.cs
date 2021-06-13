afd96f9402b5e4af230383fe8913debd649de962
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-06-13 11:25:23 +0300

    Functional programming - lab
    1. Sort even numbers
    2. Sum numbers
    3. Count uppercase

---
+++ C#/C# Advanced/FunctionalProgramming.Lab/01.SortEvenNumbers/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.SortEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] integers = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+
+            var sortedIntegers = integers.Where(x => x % 2 == 0).OrderBy(x => x).ToArray();
+
+            Console.WriteLine(string.Join(", ", sortedIntegers));
+        }
+    }
+}
---
+++ C#/C# Advanced/FunctionalProgramming.Lab/03.CountUppercaseWords/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.CountUppercaseWords
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Where(w => char.IsUpper(w.First()))
+                .ToArray();
+
+            Console.WriteLine(string.Join(Environment.NewLine, words));
+        }
+    }
+}
