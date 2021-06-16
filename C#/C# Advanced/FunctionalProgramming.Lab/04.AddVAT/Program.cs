4b0080da060d2351a6aafdb9ba5c024899c08588
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-06-16 08:28:39 +0300

    Functional programming - exercise
    4. Add VAT
    5. Filter by age

---
+++ C#/C# Advanced/FunctionalProgramming.Lab/04.AddVAT/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace _04.AddVAT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var prices = Console.ReadLine()
+                 .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                 .Select(double.Parse)
+                 .Select(x => (x *= 1.2).ToString("F2"));               
+
+            Console.WriteLine(string.Join(Environment.NewLine, prices));
+        }
+    }
+}
---
+++ C#/C# Advanced/FunctionalProgramming.Lab/05.FilterByAge/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.FilterByAge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> persons = new Dictionary<string, int>();
+
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] tokens = Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+                string name = tokens[0];
+                int age = int.Parse(tokens[1]);
+
+                AddPerson(name, age, ref persons);
+            }
+
+            string condition = Console.ReadLine();
+            int ageFilter = int.Parse(Console.ReadLine());
+            string format = Console.ReadLine();
+
+            string result = GetResult(condition, ageFilter, format, persons);
+            Console.WriteLine(result);
+        }
+
+        private static string GetResult(string condition, int ageFilter, string format, Dictionary<string, int> persons)
+        {
+            string[] filtered;
+
+            Func<KeyValuePair<string, int>, bool> conditionPredicate;
+
+            switch (condition)
+            {
+                case "younger":
+                    conditionPredicate = x => x.Value < ageFilter;
+                    break;
+                case "older":
+                    conditionPredicate = x => x.Value >= ageFilter;
+                    break;
+                default:
+                    throw new InvalidOperationException();
+            }
+
+            Func<KeyValuePair<string, int>, string> formatPredicate;
+
+            switch (format)
+            {
+                case "name":
+                    formatPredicate = x => $"{x.Key}";
+
+                    break;
+                case "age":
+                    formatPredicate = x => $"{x.Value}";
+
+                    break;
+                case "name age":
+                    formatPredicate = x => $"{x.Key} - {x.Value}";
+
+                    break;
+                default:
+                    throw new InvalidOperationException();
+            }
+
+            filtered = persons.Where(conditionPredicate).Select(formatPredicate).ToArray();
+
+            return string.Join(Environment.NewLine, filtered);
+        }
+
+        private static void AddPerson(string name, int age, ref Dictionary<string, int> persons)
+        {
+            if (!persons.ContainsKey(name))
+            {
+                persons.Add(name, age);
+            }
+        }
+    }
+}
