2e791e0265ed2b5e7980ff636f855e98178801f8
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-06-17 09:00:45 +0300

    Generics - lab
    All problems are solved!

---
+++ C#/C# Advanced/Generics.Lab/BoxOfT/Box.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxOfT
+{
+    class Box<T>
+    {
+        private List<T> box;
+
+        public Box()
+        {
+            this.box = new List<T>();
+        }
+
+        public int Count 
+        {
+            get => this.box.Count;
+        }
+
+
+        public void Add(T element)
+        {
+            this.box.Add(element);
+        }
+
+        public T Remove()
+        {
+            T element = this.box.Last();
+            this.box.Remove(element);
+            return element;
+        }
+    }
+}
---
+++ C#/C# Advanced/Generics.Lab/GenericScale/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace GenericScale
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            EqualityScale<int> equalityScale = new EqualityScale<int>(1, 10);
+            Console.WriteLine(equalityScale.AreEqual());
+
+            EqualityScale<string> str = new EqualityScale<string>("None", "None");
+            Console.WriteLine(str.AreEqual());
+        }
+    }
+}
