867cf1823c0279e562ce69c9f0f9a20eb01683a1
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-06-24 20:16:33 +0300

    Iterators and comparators - lab
    3rd problem solved

---
+++ C#/C# Advanced/IteratorsAndComparators.Lab/03.ComparableBook/Library.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private SortedSet<Book> books;
+
+        public Library(params Book[] books)
+        {
+            this.books = new SortedSet<Book>(books);
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return new LibraryIterator(this.books);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        private class LibraryIterator : IEnumerator<Book>
+        {
+            private readonly IList<Book> books;
+            private int currentIndex = -1;
+
+            public LibraryIterator(IEnumerable<Book> books)
+            {
+                Reset();
+                this.books = new List<Book>(books);
+            }
+
+
+            public Book Current => this.books[this.currentIndex];
+            object IEnumerator.Current => this.Current;
+
+
+            public void Dispose()
+            {
+                this.books.Clear();
+            }
+
+            public bool MoveNext()
+            {
+                return ++this.currentIndex < this.books.Count;
+            }
+
+            public void Reset()
+            {
+                this.currentIndex = -1;
+            }
+        }
+
+    }
+
+}
