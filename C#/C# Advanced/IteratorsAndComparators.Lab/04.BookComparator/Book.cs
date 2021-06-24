e2aa630922398eb309d29e0e1919e4a736fa9f4a
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-06-24 20:15:50 +0300

    Iterators and comparators - lab
    4 problem solved

---
+++ C#/C# Advanced/IteratorsAndComparators.Lab/04.BookComparator/Book.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace IteratorsAndComparators
+{
+    public class Book : IComparable<Book>
+    {
+        public Book(string title, int year, params string[] authors)
+        {
+            this.Title = title;
+            this.Year = year;
+            this.Authors = new List<string>(authors);
+        }
+
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public List<string> Authors { get; set; }
+
+        public int CompareTo([AllowNull] Book other)
+        {
+            if (this.Year != other.Year)
+            {
+                return this.Year.CompareTo(other.Year);
+            }
+
+            if (this.Title != other.Title)
+            {
+                return this.Title.CompareTo(other.Title);
+            }
+
+            return 0;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Title} - {this.Year}";
+        }
+    }
+}
