848da242ea097d198d7ab959c64918ce96b4f378
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-06-26 10:58:48 +0300

    Iterators and comparators - exercise
    1. Listy iterator

---
+++ C#/C# Advanced/IteratorsAndComparators.Exercise/01.ListyIterator/ListyIterator.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _01.ListyIterator
+{
+    class ListyIterator<T>
+    {
+        private List<T> collection;
+        private int currentIndex = 0;
+
+        public ListyIterator(params T[] items)
+        {
+            this.collection = new List<T>(items);
+        }
+
+        public bool Move()
+        {
+            bool canMove = HasNext();
+
+            if (canMove)
+            {
+                this.currentIndex++;
+            }
+
+            return canMove;
+        }
+
+        public bool HasNext()
+        {
+            if (this.currentIndex + 1 < this.collection.Count)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public void Print()
+        {
+            if (this.collection.Count == 0)
+            {
+                throw new InvalidOperationException("Invalid Operation!");
+            }
+
+            Console.WriteLine(this.collection[this.currentIndex]);
+        }
+    }
+}
---
+++ C#/C# Advanced/IteratorsAndComparators.Exercise/01.ListyIterator/StartUp.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.ListyIterator
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string createCommand = Console.ReadLine();
+            ListyIterator<string> listy = Create(createCommand);
+
+            try
+            {
+                string command;
+                while ((command = Console.ReadLine()) != "END")
+                {
+                    switch (command)
+                    {
+                        case "Move":
+                            Console.WriteLine(listy.Move());
+                            break;
+                        case "HasNext":
+                            Console.WriteLine(listy.HasNext());
+                            break;
+                        case "Print":
+                            listy.Print();
+                            break;
+
+                        default:
+                            throw new InvalidOperationException("Invalid command!");
+                    }
+                }
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private static ListyIterator<string> Create(string createCommand)
+        {
+            string[] tokens = createCommand.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            if (tokens[0] != "Create")
+            {
+                throw new InvalidOperationException("Invalid command!");
+            }
+
+            string[] items = tokens.Skip(1).ToArray();
+
+            return new ListyIterator<string>(items);
+        }
+    }
+}
