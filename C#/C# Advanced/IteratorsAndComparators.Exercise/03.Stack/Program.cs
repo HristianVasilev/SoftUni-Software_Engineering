7b21e0e10bb244ec0e9fdd2e7e9795c91b242997
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-06-26 11:00:55 +0300

    Iterators and comparators - exercise
    3. Stack

---
+++ C#/C# Advanced/IteratorsAndComparators.Exercise/03.Stack/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+
+            string input;
+            while ((input = Console.ReadLine()) != "END")
+            {
+                string[] tokens = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                switch (tokens[0])
+                {
+                    case "Push":
+                        int[] integers = tokens
+                            .Skip(1)
+                            .Select(x => x.Trim(','))
+                            .Select(int.Parse)
+                            .ToArray();
+
+                        foreach (var i in integers)
+                        {
+                            stack.Push(i);
+                        }
+
+                        break;
+                    case "Pop":
+                        try
+                        {
+                            stack.Pop();
+                        }
+                        catch (ArgumentException e)
+                        {
+                            Console.WriteLine(e.Message);
+                        }
+
+                        break;
+
+                    default:
+                        throw new InvalidOperationException("Invalid command!");
+                }
+            }
+
+            Foreach(stack);
+            Foreach(stack);
+
+        }
+
+        private static void Foreach(Stack<int> stack)
+        {
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
---
+++ C#/C# Advanced/IteratorsAndComparators.Exercise/03.Stack/Stack.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.Stack
+{
+    class Stack<T> : IEnumerable<T>
+    {
+        private List<T> collection;
+
+        public Stack()
+        {
+            this.collection = new List<T>();
+        }
+
+        public void Push(T element)
+        {
+            this.collection.Add(element);
+        }
+
+        public T Pop()
+        {
+            if (this.collection.Count == 0)
+            {
+                throw new System.ArgumentException("No elements");
+            }
+
+            T element = this.collection.LastOrDefault();
+            this.collection.RemoveAt(this.collection.Count - 1);
+
+            return element;
+        }
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = this.collection.Count - 1; i >= 0; i--)
+            {
+                yield return this.collection[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+
+    }
+}
