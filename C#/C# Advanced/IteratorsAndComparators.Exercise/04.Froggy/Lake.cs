cde0a2c6a8faec386e5048147ed4859167e006c4
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-06-26 11:01:38 +0300

    Iterators and comparators - exercise
    4. Froggy

---
+++ C#/C# Advanced/IteratorsAndComparators.Exercise/04.Froggy/Lake.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _04.Froggy
+{
+    class Lake : IEnumerable<int>
+    {
+        private int[] stones;
+
+        public Lake(int[] stones)
+        {
+            this.stones = stones;
+        }
+
+        public void OrderStones()
+        {
+            List<int> even = new List<int>();
+            List<int> odd = new List<int>();
+
+            for (int i = 0; i < this.stones.Length; i++)
+            {
+                if (i % 2 == 0)
+                {
+                    even.Add(this.stones[i]);
+                }
+                else
+                {
+                    odd.Add(this.stones[i]);
+                }
+            }
+
+            List<int> newStonesCollections = new List<int>();
+
+            odd.Reverse();
+
+            newStonesCollections.AddRange(even);
+            newStonesCollections.AddRange(odd);
+
+            this.stones = newStonesCollections.ToArray();
+        }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            foreach (var stone in this.stones)
+            {
+                yield return stone;
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator() => this.GetEnumerator();
+
+    }
+}
---
+++ C#/C# Advanced/IteratorsAndComparators.Exercise/04.Froggy/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _04.Froggy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] stones = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Lake lake = new Lake(stones);
+            lake.OrderStones();
+
+            string result = GetResult(lake);
+            Console.WriteLine(result);
+        }
+
+        private static string GetResult(Lake lake)
+        {
+            List<int> res = new List<int>();
+
+            foreach (var stone in lake)
+            {
+                res.Add(stone);
+            }
+
+            return string.Join(", ", res);
+        }
+    }
+}
