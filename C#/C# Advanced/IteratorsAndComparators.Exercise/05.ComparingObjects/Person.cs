62c271e807f3c6afe7146fb302278b567bfeb9b9
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-06-26 11:21:41 +0300

    Iterators and comparators - exercise
    5. Comparing objects

---
+++ C#/C# Advanced/IteratorsAndComparators.Exercise/05.ComparingObjects/Person.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace _05.ComparingObjects
+{
+    class Person : IComparable<Person>
+    {
+        public Person(string name, int age, string town)
+        {
+            this.Name = name;
+            this.Age = age;
+            this.Town = town;
+        }
+
+        public string Name { get; private set; }
+        public int Age { get; private set; }
+        public string Town { get; private set; }
+
+        public int CompareTo([AllowNull] Person other)
+        {
+            int result = this.Name.CompareTo(other.Name);
+
+            if (result == 0)
+            {
+                result = this.Age.CompareTo(other.Age);
+
+                if (result == 0)
+                {
+                    result = this.Town.CompareTo(other.Town);
+                }
+            }
+
+            return result;
+        }
+
+        public override bool Equals(object obj)
+        {
+            Person person = obj as Person;
+
+            if (person == null)
+            {
+                throw new InvalidCastException("The object is different than Person type!");
+            }
+
+            if (CompareTo(person) == 0)
+            {
+                return true;
+            }
+
+            return false;
+        }
+    }
+}
---
+++ C#/C# Advanced/IteratorsAndComparators.Exercise/05.ComparingObjects/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _05.ComparingObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person[] people = ReadPeople();
+
+            int index = int.Parse(Console.ReadLine());
+
+            string result = GetResult(people, index);
+            Console.WriteLine(result);
+        }
+
+        private static string GetResult(Person[] people, int index)
+        {
+            Person matchPerson = people[index - 1];
+
+            int countOfMatches = 0;
+            int countOfNonMatches = 0;
+
+            foreach (var person in people)
+            {
+                if (person.Equals(matchPerson))
+                {
+                    countOfMatches++;
+                }
+                else
+                {
+                    countOfNonMatches++;
+                }
+            }
+
+            if (countOfMatches - 1 == 0)
+            {
+                return "No matches";
+            }
+
+            int totalPeople = people.Length;
+
+            return $"{countOfMatches} {countOfNonMatches} {totalPeople}";
+        }
+
+        private static Person[] ReadPeople()
+        {
+            List<Person> people = new List<Person>();
+
+            string input;
+            while ((input = Console.ReadLine()) != "END")
+            {
+                string[] tokens = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string name = tokens[0];
+                int age = int.Parse(tokens[1]);
+                string town = tokens[2];
+
+                Person person = new Person(name, age, town);
+                people.Add(person);
+            }
+
+            return people.ToArray();
+        }
+    }
+}
