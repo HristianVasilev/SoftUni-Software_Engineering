28b0642fd7592e190c9b663a22b2204bf68107e5
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-06-29 20:00:34 +0300

    exam prep

---
+++ C#/C# Advanced/Exam/Program/CSharpAdvancedExam26June21/01.Masterchef/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _01.Masterchef
+{
+    class Program
+    {
+        private static Dictionary<string, int> dishes;
+        private static Dictionary<string, int> preparedDishes;
+
+        static void Main(string[] args)
+        {
+            dishes = new Dictionary<string, int>
+            {
+                {"Dipping sauce", 150},
+                {"Green salad", 250},
+                {"Chocolate cake", 300},
+                {"Lobster", 400}
+            };
+            preparedDishes = new Dictionary<string, int>();
+
+            Queue<int> numberOfIngredients = new Queue<int>(Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse));
+
+            Stack<int> freshnessLevel = new Stack<int>(Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse));
+
+            Cook(ref numberOfIngredients, ref freshnessLevel);
+
+            string result = GetResult(numberOfIngredients);
+            Console.WriteLine(result);
+        }
+
+        private static string GetResult(Queue<int> numberOfIngredients)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            if (dishes.All(x => preparedDishes.ContainsKey(x.Key)))
+            {
+                sb.AppendLine("Applause! The judges are fascinated by your dishes!");
+            }
+            else
+            {
+                sb.AppendLine("You were voted off. Better luck next year.");
+            }
+
+            if (numberOfIngredients.Count != 0)
+            {
+                int ingridientsSum = numberOfIngredients.Sum();
+                sb.AppendLine($"Ingredients left: {ingridientsSum}");
+            }
+
+            Dictionary<string, int> collection = preparedDishes
+                .OrderBy(x => x.Key)
+                .ToDictionary(k => k.Key, v => v.Value);
+
+            foreach (var dish in collection)
+            {
+                sb.AppendLine($" # {dish.Key} --> {dish.Value}");
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+        private static void Cook(ref Queue<int> numberOfIngredients, ref Stack<int> freshnessLevel)
+        {
+            while (numberOfIngredients.Count != 0 && freshnessLevel.Count != 0)
+            {
+                int ingredient = numberOfIngredients.Peek();
+                int freshness = freshnessLevel.Peek();
+
+                if (ingredient == 0)
+                {
+                    numberOfIngredients.Dequeue();
+                    continue;
+                }
+
+                int product = ingredient * freshness;
+
+                if (dishes.Any(v => v.Value.Equals(product)))
+                {
+                    numberOfIngredients.Dequeue();
+                    freshnessLevel.Pop();
+
+                    string dish = dishes.First(x => x.Value.Equals(product)).Key;
+                    AddDish(dish);
+                }
+                else
+                {
+                    freshnessLevel.Pop();
+                    numberOfIngredients.Enqueue(numberOfIngredients.Dequeue() + 5);
+                }
+            }
+        }
+
+        private static void AddDish(string dish)
+        {
+            if (!preparedDishes.ContainsKey(dish))
+            {
+                preparedDishes.Add(dish, 0);
+            }
+
+            preparedDishes[dish]++;
+        }
+    }
+}
---
+++ C#/C# Advanced/Exam/Program/CSharpAdvancedExam26June21/02.Survivor/Program.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace _02.Survivor
+{
+    class Program
+    {
+        private static char[][] jaggedArray;
+        private static int collectedTokens;
+        private static int opponentTokens;
+
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+            jaggedArray = ReadArray(rows);
+
+            collectedTokens = 0;
+            opponentTokens = 0;
+
+            string command;
+            while ((command = Console.ReadLine()) != "Gong")
+            {
+                string[] tokens = command.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                string action = tokens[0];
+                int row = int.Parse(tokens[1]);
+                int col = int.Parse(tokens[2]);
+
+                if (!ValidCoordinates(row, col))
+                {
+                    continue;
+                }
+
+                switch (action)
+                {
+                    case "Find":
+                        if (jaggedArray[row][col].Equals('T'))
+                        {
+                            collectedTokens = CollectToken(row, col, collectedTokens);
+                        }
+
+                        break;
+                    case "Opponent":
+                        if (jaggedArray[row][col].Equals('T'))
+                        {
+                            opponentTokens = CollectToken(row, col, opponentTokens);
+                        }
+
+                        string direction = tokens[3];
+                        Move(row, col, direction);
+
+
+                        break;
+
+                    default:
+                        throw new ArgumentException();
+                }
+            }
+
+            string result = GetResult();
+            Console.WriteLine(result);
+        }
+
+        private static string GetResult()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                sb.AppendLine($"{string.Join(' ', jaggedArray[i])}");
+            }
+
+            sb.AppendLine($"Collected tokens: {collectedTokens}");
+            sb.AppendLine($"Opponent's tokens: {opponentTokens}");
+
+            return sb.ToString().TrimEnd();
+        }
+
+        private static void Move(int row, int col, string direction)
+        {
+            switch (direction)
+            {
+                case "up":
+                    for (int i = 0; i < 3; i++)
+                    {
+                        row--;
+
+                        if (!ValidCoordinates(row, col))
+                        {
+                            break;
+                        }
+
+                        if (jaggedArray[row][col].Equals('T'))
+                        {
+                            opponentTokens = CollectToken(row, col, opponentTokens);
+                        }
+                    }
+
+                    break;
+                case "down":
+                    for (int i = 0; i < 3; i++)
+                    {
+                        row++;
+
+                        if (!ValidCoordinates(row, col))
+                        {
+                            break;
+                        }
+
+                        if (jaggedArray[row][col].Equals('T'))
+                        {
+                            opponentTokens = CollectToken(row, col, opponentTokens);
+                        }
+                    }
+
+                    break;
+                case "left":
+                    for (int i = 0; i < 3; i++)
+                    {
+                        col--;
+
+                        if (!ValidCoordinates(row, col))
+                        {
+                            break;
+                        }
+
+                        if (jaggedArray[row][col].Equals('T'))
+                        {
+                            opponentTokens = CollectToken(row, col, opponentTokens);
+                        }
+                    }
+
+                    break;
+                case "right":
+                    for (int i = 0; i < 3; i++)
+                    {
+                        col++;
+
+                        if (!ValidCoordinates(row, col))
+                        {
+                            break;
+                        }
+
+                        if (jaggedArray[row][col].Equals('T'))
+                        {
+                            opponentTokens = CollectToken(row, col, opponentTokens);
+                        }
+                    }
+
+                    break;
+
+                default:
+                    throw new ArgumentException();
+            }
+        }
+
+        private static int CollectToken(int row, int col, int collect)
+        {
+            jaggedArray[row][col] = '-';
+
+            return ++collect;
+        }
+
+        private static bool ValidCoordinates(int row, int col)
+        {
+            return row >= 0 && row < jaggedArray.Length && col >= 0 && col < jaggedArray[row].Length;
+        }
+
+        private static char[][] ReadArray(int rows)
+        {
+            char[][] jaggedArray = new char[rows][];
+
+            for (int i = 0; i < rows; i++)
+            {
+                jaggedArray[i] = Console.ReadLine()
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(char.Parse)
+                    .ToArray();
+            }
+
+            return jaggedArray;
+        }
+
+        private static void PrintMatrix()
+        {
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                Console.WriteLine(string.Join(' ', jaggedArray[i]));
+            }
+        }
+    }
+}
