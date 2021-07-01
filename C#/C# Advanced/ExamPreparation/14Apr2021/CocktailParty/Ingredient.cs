c8f5a653f6bc7f41284f74fde6d7bccb667a31eb
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-07-01 21:03:03 +0300

    Exam

---
+++ C#/C# Advanced/ExamPreparation/14Apr2021/CocktailParty/Ingredient.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace CocktailParty
+{
+    class Ingredient : IComparable<Ingredient>
+    {
+        public Ingredient(string name, int alcohol, int quantity)
+        {
+            this.Name = name;
+            this.Alcohol = alcohol;
+            this.Quantity = quantity;
+        }
+
+        public string Name { get; set; }
+        public int Alcohol { get; set; }
+        public int Quantity { get; set; }
+
+        public int CompareTo([AllowNull] Ingredient other)
+        {
+            return this.Name.CompareTo(other.Name);
+        }
+
+        public override string ToString()
+        {
+            return $"Ingredient: {this.Name}{Environment.NewLine}Quantity: {this.Quantity}{Environment.NewLine}Alcohol: {this.Alcohol}";
+        }
+    }
+}
