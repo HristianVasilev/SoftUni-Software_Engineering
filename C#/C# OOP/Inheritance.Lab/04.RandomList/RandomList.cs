cdf61b74602e51820025614bdb91d75913b38142
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-07-06 18:20:11 +0300

    Inheritance - lab
    Random list

---
+++ C#/C# OOP/Inheritance.Lab/04.RandomList/RandomList.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CustomRandomList
+{
+    class RandomList : List<string>
+    {
+        public string RandomString()
+        {
+            Random rnd = new Random();
+            int randomIndex = rnd.Next(0, base.Count - 1);
+
+            string randomElement = base[randomIndex];
+            base.RemoveAt(randomIndex);
+
+            return randomElement;
+        }
+
+    }
+}
---
+++ C#/C# OOP/Inheritance.Lab/04.RandomList/StartUp.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CustomRandomList
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            RandomList randomList = new RandomList();
+
+            randomList.Add("AAA");
+            randomList.Add("BBB");
+            randomList.Add("CCC");
+            randomList.Add("DDD");
+            randomList.Add("EEE");
+            randomList.Add("FFF");
+
+            Console.WriteLine(randomList.RandomString());
+        }
+    }
+}
