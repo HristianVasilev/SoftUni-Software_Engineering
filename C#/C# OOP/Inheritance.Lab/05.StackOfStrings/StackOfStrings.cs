ba9959420bf41b593c386d381e3fe94bb7c48286
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-07-06 22:16:15 +0300

    Inheritance - lab
    All completed!

---
+++ C#/C# OOP/Inheritance.Lab/05.StackOfStrings/StackOfStrings.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace CustomStack
+{
+    class StackOfStrings : Stack<string>
+    {
+        public bool IsEmpty()
+        {
+            return base.Count == 0;
+        }
+
+        public void AddRange(ICollection<string> collection)
+        {
+            foreach (var item in collection)
+            {
+                base.Push(item);
+            }
+        }
+
+    }
+}
---
+++ C#/C# OOP/Inheritance.Lab/05.StackOfStrings/StartUp.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace CustomStack
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            StackOfStrings stackOfStrings = new StackOfStrings();
+
+            Console.WriteLine(stackOfStrings.IsEmpty());
+            stackOfStrings.AddRange(new string[] { "A", "B", "C" });
+            Console.WriteLine(stackOfStrings.IsEmpty());
+        }
+    }
+}
