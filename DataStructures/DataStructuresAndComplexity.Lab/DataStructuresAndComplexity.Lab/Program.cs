db4b5c15bf5b601dbc1c083b0793a5703749a322
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-07-06 22:16:43 +0300

    Data structures - complexity
    Start data structures

---
+++ DataStructures/DataStructuresAndComplexity.Lab/DataStructuresAndComplexity.Lab/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace DataStructuresAndComplexity.Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        long GetOperationsCount(int n)
+        {
+            long counter = 0;
+            for (int i = 0; i < n; i++)
+                for (int j = 0; j < n; j++)
+                    counter++;
+            return counter;
+        }
+
+    }
+}
