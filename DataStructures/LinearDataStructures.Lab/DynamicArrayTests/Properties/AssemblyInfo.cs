e7ae10fcefabab3263ee0dd3cd930cf4e3165593
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-07-08 18:12:01 +0300

    Linear data structures

---
+++ DataStructures/LinearDataStructures.Lab/DynamicArrayTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("DynamicArrayTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("DynamicArrayTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("db437bdf-7efd-49bd-a4ee-b41b2199fff8")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ DataStructures/LinearDataStructures.Lab/DynamicArrayTests/UnitTest1.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using System;
+
+namespace DynamicArrayTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private static DynamicArr<int> dynamicArr; 
+
+        [SetUp]
+        public UnitTest1()
+        {
+
+        }
+
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
