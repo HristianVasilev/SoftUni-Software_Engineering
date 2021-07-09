0b1713a5e868c91a51be70035c9ea5be70e395fd
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-07-09 17:56:51 +0300

    Data structures
    Implement queue

---
+++ DataStructures/LinearDataStructures.Lab/Queue/Queue.cs
@@ -0,0 +1,92 @@
+﻿namespace Queue
+{
+    class Queue<T>
+    {
+        class Node<T>
+        {
+            public Node(T element)
+            {
+                this.Element = element;
+            }
+
+            public T Element { get; set; }
+            public Node<T> Next { get; set; }
+        }
+
+        private Node<T> head;
+        private int size;
+
+        public Queue()
+        {
+
+        }
+
+        public int Count => this.size;
+
+        public void Clear()
+        {
+            this.head = null;
+            this.size = 0;
+        }
+
+        public bool Contains(T item)
+        {
+            Node<T> currentNode = this.head;
+
+            while (currentNode != null)
+            {
+                if (currentNode.Element.Equals(item))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+
+        public void Enqueue(T item)
+        {
+            Node<T> newNode = new Node<T>(item);
+
+            if (this.head == null)
+            {
+                this.head = newNode;
+            }
+            else
+            {
+                Node<T> current = this.head;
+
+                while (current.Next != null)
+                {
+                    current = current.Next;
+                }
+
+                current.Next = newNode;
+            }
+
+            this.size++;
+        }
+
+        public T Dequeue()
+        {
+            Node<T> first = this.head;
+
+            if (first is null)
+            {
+                throw new System.InvalidOperationException("Queue is empty!");
+            }
+
+            this.head = this.head.Next;
+            this.size--;
+
+            return first.Element;
+        }
+
+        public T Peek()
+        {
+            return this.head.Element;
+        }
+
+    }
+}
---
+++ DataStructures/LinearDataStructures.Lab/Stack/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Stack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Collections.Generic.ICollection<int> numbers = new int[] { 10, 20, 30 };
+
+            Stack<int> stack = new Stack<int>(numbers);
+
+            var stackCloned = stack.Clone();
+
+            foreach (var item in stack)
+            {
+                Console.WriteLine(item);
+            }
+
+            Console.WriteLine(string.Join(", ",stack.ToArray()));
+
+
+            //Console.WriteLine($"Stack valid contains: {stack.Contains(10)}");
+            //Console.WriteLine($"Stack valid contains: {stack.Contains(20)}");
+            //Console.WriteLine($"Stack valid contains: {stack.Contains(30)}");
+
+            //Console.WriteLine($"Stack invalid contains: {stack.Contains(300)}");
+            //;
+
+            //Console.WriteLine($"Stack count: {stack.Count}");
+            //Console.WriteLine($"Stack's top element: {stack.Peek()}");
+
+            //while (stack.Count != 0)
+            //{
+            //   Console.WriteLine(stack.Pop());
+            //   Console.WriteLine($"stack count: {stack.Count}");
+            //}
+        }
+    }
+}
