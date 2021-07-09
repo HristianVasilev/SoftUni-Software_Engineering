84e252a72a9cc66a35be0c418fa4ebfba774eb90
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-07-09 17:56:21 +0300

    Data structures
    dynamic array

---
+++ DataStructures/LinearDataStructures.Lab/Queue/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<int> queue = new Queue<int>();
+
+            queue.Enqueue(5);
+            queue.Enqueue(6);
+            queue.Enqueue(7);
+            queue.Enqueue(8);
+
+            Console.WriteLine($"Queue count is: {queue.Count}");
+            Console.WriteLine($"Queue peek: {queue.Peek()}");
+            //Console.WriteLine($"Queue dequeue is: {queue.Dequeue()}");
+            //Console.WriteLine($"Queue dequeue is: {queue.Dequeue()}");
+            //Console.WriteLine($"Queue dequeue is: {queue.Dequeue()}");
+            //Console.WriteLine($"Queue dequeue is: {queue.Dequeue()}");
+            //Console.WriteLine($"Queue dequeue is: {queue.Dequeue()}");
+        }
+    }
+}
