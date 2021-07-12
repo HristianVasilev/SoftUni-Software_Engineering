d70d25971fd2830bb02909655d64d48a6eb9a039
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-07-12 07:55:13 +0300

    Person task
    Completed

---
+++ C#/C# OOP/Inheritance.Exercise/Person/Child.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Person
+{
+    public class Child : Person
+    {
+        private const int minAge = 0;
+        private const int maxAge = 15;
+
+        public Child(string name, int age) : base(name, age)
+        {
+          
+        }
+
+        private void ValidAge(int age)
+        {
+            if (age >= minAge && age <= maxAge)
+            {
+                return;
+            }
+
+            throw new ArgumentException("Child's age is invalid!");
+        }
+    }
+}
---
+++ C#/C# OOP/Inheritance.Exercise/Person/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Person
+{
+    public class Person
+    {
+        private string name;
+        private int age;
+
+        public Person(string name, int age)
+        {
+            this.Name = name;
+            this.Age = age;
+        }
+
+        public string Name
+        {
+            get => this.name;
+            private set => this.name = value;
+        }
+
+        public int Age
+        {
+            get => this.age;
+            private set 
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Age cannot be a negative number!");
+                }
+
+                this.age = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Name: {this.Name}, Age: {this.Age}";
+        }
+    }
+}
