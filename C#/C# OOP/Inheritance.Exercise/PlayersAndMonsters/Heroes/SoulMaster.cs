2a169d583220f9cbcaf65b646063ee271e664c67
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-07-16 08:13:08 +0300

    Heroes task

---
+++ C#/C# OOP/Inheritance.Exercise/PlayersAndMonsters/Heroes/SoulMaster.cs
@@ -0,0 +1,9 @@
+﻿namespace PlayersAndMonsters.Heroes
+{
+    public class SoulMaster : DarkWizard
+    {
+        public SoulMaster(string username, int level) : base(username, level)
+        {
+        }
+    }
+}
