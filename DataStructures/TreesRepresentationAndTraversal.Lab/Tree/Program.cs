08c3d1e5211912ea0abe80718039186067fec5c6
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-07-17 20:57:50 +0300

    Data structures - tree

---
+++ DataStructures/TreesRepresentationAndTraversal.Lab/Tree/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree<int> tree = new Tree<int>(
+                7,
+                    new Tree<int>(19,
+                        new Tree<int>(1),
+                        new Tree<int>(12),
+                        new Tree<int>(31)
+                     ),
+                    new Tree<int>(21),
+                    new Tree<int>(14,
+                        new Tree<int>(23),
+                        new Tree<int>(6)
+                     )
+                    );
+            //** BFS Traversal
+
+            //var result = tree.OrderBfs();
+            //Console.WriteLine(string.Join(", ", result));
+
+            //** DFS Traversal
+
+            //var result = tree.OrderDfs();
+            //Console.WriteLine(string.Join(", ", result));
+
+            //** Add Child
+            //tree.AddChild(21, new Tree<int>(55));
+
+            //** Remove node
+
+            //tree.RemoveNode(129);
+            //;
+
+            tree.Swap(19, 14);
+        }
+    }
+}
---
+++ DataStructures/TreesRepresentationAndTraversal.Lab/Tree/Tree.cs
@@ -0,0 +1,192 @@
+﻿namespace Tree
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class Tree<T> : IAbstractTree<T>
+    {
+        private readonly List<Tree<T>> children;
+        private bool IsRootDeleted;
+
+        public Tree(T value)
+        {
+            this.Value = value;
+            this.Parent = this;
+            children = new List<Tree<T>>();
+            this.IsRootDeleted = false;
+        }
+
+        public Tree(T value, params Tree<T>[] children)
+            : this(value)
+        {
+            this.children = children.ToList();
+        }
+
+
+        public T Value { get; private set; }
+        public Tree<T> Parent { get; private set; }
+        public IReadOnlyCollection<Tree<T>> Children => this.children.AsReadOnly();
+
+
+        public ICollection<T> OrderBfs()
+        {
+            List<T> result = new List<T>();
+
+            if (IsRootDeleted)
+            {
+                return result;
+            }
+
+            Queue<Tree<T>> queue = new Queue<Tree<T>>();
+            queue.Enqueue(this);
+
+            while (queue.Count != 0)
+            {
+                Tree<T> subTree = queue.Dequeue();
+
+                foreach (var item in subTree.children)
+                {
+                    queue.Enqueue(item);
+                }
+
+                result.Add(subTree.Value);
+            }
+
+            return result;
+        }
+
+        public ICollection<T> OrderDfs()
+        {
+            List<T> result = new List<T>();
+
+            if (IsRootDeleted)
+            {
+                return result;
+            }
+
+            //**Using stack!
+            Stack<Tree<T>> stack = new Stack<Tree<T>>();
+            stack.Push(this);
+
+            while (stack.Count != 0)
+            {
+                Tree<T> subTree = stack.Pop();
+
+                foreach (var child in subTree.Children)
+                {
+                    stack.Push(child);
+                }
+
+                result.Add(subTree.Value);
+            }
+
+            result.Reverse();
+
+            //** Using recursion!
+
+            return result;
+        }
+
+        public void AddChild(T parentKey, Tree<T> child)
+        {
+            //*** Using FindBFS
+
+            //Tree<T> searchedNode = this.FindBFS(parentKey);
+
+            //if (searchedNode == null)
+            //{
+            //    throw new ArgumentNullException();
+            //}
+            //searchedNode.children.Add(child);
+
+            //*** Using FindDFS
+            Tree<T> searchedNode = this.FindDFS(this, parentKey);
+
+            if (searchedNode == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            searchedNode.children.Add(child);
+        }
+
+        public void RemoveNode(T nodeKey)
+        {
+            Tree<T> searchedNode = FindBFS(nodeKey);
+
+            if (searchedNode == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            if (this.Value.Equals(nodeKey))
+            {
+                this.Value = default(T);
+                this.Parent = null;
+                this.children.Clear();
+                this.IsRootDeleted = true;
+                return;
+            }
+
+            this.children.Remove(searchedNode);
+        }
+
+        public void Swap(T firstKey, T secondKey)
+        {
+            Tree<T> firstNode = FindBFS(firstKey);
+            Tree<T> secondNode = FindBFS(secondKey);
+
+            if (firstNode == null || secondNode == null)
+            {
+                throw new ArgumentNullException();
+            }
+
+            Tree<T> tempNode = firstNode;
+            firstNode = secondNode;
+            secondNode = tempNode;
+        }
+
+        private Tree<T> FindBFS(T nodeKey)
+        {
+            Queue<Tree<T>> queue = new Queue<Tree<T>>();
+            queue.Enqueue(this);
+
+            while (queue.Count != 0)
+            {
+                Tree<T> tree = queue.Dequeue();
+
+                if (tree.Value.Equals(nodeKey))
+                {
+                    return tree;
+                }
+
+                foreach (var child in tree.Children)
+                {
+                    queue.Enqueue(child);
+                }
+            }
+
+            return null;
+        }
+
+        private Tree<T> FindDFS(Tree<T> node, T nodeKey)
+        {
+            if (node.Value.Equals(nodeKey))
+            {
+                return node;
+            }
+
+            foreach (var child in node.Children)
+            {
+                var tree = FindDFS(child, nodeKey);
+                if (tree != null)
+                {
+                    return tree;
+                }
+            }
+
+            return null;
+        }
+    }
+}
