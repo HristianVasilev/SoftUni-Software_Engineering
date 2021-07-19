5bb9c66f95d690f9e37fea83345203e6f2e7c149
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-07-19 19:59:58 +0300

    Encapsulation exercise

---
+++ C#/C# OOP/Encapsulation.Exercise/AnimalFarm/01.ClassBoxData/Models/Box.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace _01.ClassBoxData
+{
+    class Box
+    {
+        private double length;
+        private double width;
+        private double height;
+
+        public Box(double length, double width, double height)
+        {
+            this.Length = length;
+            this.Width = width;
+            this.Height = height;
+        }
+
+        public double Length
+        {
+            get => this.length;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Length cannot be zero or negative.");
+                }
+
+                this.length = value;
+            }
+        }
+
+        public double Width
+        {
+            get => this.width;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Width cannot be zero or negative.");
+                }
+
+                this.width = value;
+            }
+        }
+
+        public double Height
+        {
+            get => this.height;
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Height cannot be zero or negative.");
+                }
+
+                this.height = value;
+            }
+        }
+
+        public double GetSurfaceArea()
+        {
+            return 2 * (this.Length * this.Height + this.Width * this.Height + this.Length * this.Width);
+        }
+
+        public double GetLateralSurfaceArea()
+        {
+            return 2 * (this.Length * this.Height + this.Width * this.Height);
+        }
+
+        public double GetVolume()
+        {
+            return this.Length * this.Width * this.Height;
+        }
+
+        public override string ToString()
+        {
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+
+            sb.AppendLine($"Surface Area - {this.GetSurfaceArea():f2}");
+            sb.AppendLine($"Lateral Surface Area - {this.GetLateralSurfaceArea():f2}");
+            sb.AppendLine($"Volume - {this.GetVolume():f2}");
+
+            return sb.ToString().TrimEnd();
+        }
+
+    }
+}
