7205b5f500624ee965742ca32a232473ce8e6c5e
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-07-21 20:24:41 +0300

    Validation of data

---
+++ C#/C# OOP/Encapsulation.Lab/03.ValidationOfData/Person.cs
@@ -0,0 +1,97 @@
+﻿using System;
+
+namespace PersonsInfo
+{
+    class Person
+    {
+        private const decimal minSalary = 460;
+
+        private string firstName;
+        private string lastName;
+        private int age;
+        private decimal salary;
+
+        public Person(string firstName, string lastName, int age)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Age = age;
+        }
+
+        public Person(string firstName, string lastName, int age, decimal salary) : this(firstName, lastName, age)
+        {
+            this.Salary = salary;
+        }
+
+        public string FirstName
+        {
+            get { return firstName; }
+            private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("First name cannot contain fewer than 3 symbols!");
+                }
+
+                firstName = value;
+            }
+        }
+
+        public string LastName
+        {
+            get { return lastName; }
+            private set
+            {
+                if (value.Length < 3)
+                {
+                    throw new ArgumentException("Last name cannot contain fewer than 3 symbols!");
+                }
+
+                lastName = value;
+            }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            private set
+            {
+                if (value <= 0)
+                {
+                    throw new ArgumentException("Age cannot be zero or a negative integer!");
+                }
+
+                age = value;
+            }
+        }
+
+        public decimal Salary
+        {
+            get { return salary; }
+            private set
+            {
+                if (value < minSalary)
+                {
+                    throw new ArgumentException($"Salary cannot be less than {minSalary} leva!");
+                }
+
+                salary = value;
+            }
+        }
+
+        public void IncreaseSalary(decimal percentage)
+        {
+            if (this.Age < 30)
+            {
+                percentage *= 0.5m;
+            }
+
+            this.Salary += this.Salary * percentage/100m;
+        }
+
+        public override string ToString()
+        {
+            return $"{this.FirstName} {this.LastName} gets {this.Salary:f2} leva.";
+        }
+    }
+}
