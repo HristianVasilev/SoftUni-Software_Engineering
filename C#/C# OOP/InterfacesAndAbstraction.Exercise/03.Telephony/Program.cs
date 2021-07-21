c3b416db634d91352f4a595d29bba4d231d3d792
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-07-21 20:26:57 +0300

    Telephony

---
+++ C#/C# OOP/InterfacesAndAbstraction.Exercise/03.Telephony/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.Telephony
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] phoneNumbers = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+            string[] webAdresses = Console.ReadLine().Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+            CallNumbers(phoneNumbers);
+            BrowseWeb(webAdresses);
+        }
+
+        private static void BrowseWeb(string[] webAdresses)
+        {
+            foreach (var url in webAdresses)
+            {
+                string result;
+
+                try
+                {
+                    if (url.Any(c => char.IsDigit(c)))
+                    {
+                        throw new ArgumentException("Invalid URL!");
+                    }
+
+                    IBrowsable phone = new Smartphone();
+                    result = phone.Browse(url);
+                }
+                catch (ArgumentException ex)
+                {
+                    result = ex.Message;
+                }
+
+                Console.WriteLine(result);
+            }
+        }
+
+        private static void CallNumbers(string[] phoneNumbers)
+        {
+            foreach (var phoneNumber in phoneNumbers)
+            {
+                string result;
+
+                try
+                {
+
+                    if (phoneNumber.Any(c => !char.IsDigit(c)))
+                    {
+                        throw new ArgumentException("Invalid number!");
+                    }
+
+                    ICallable phone;
+
+                    if (phoneNumber.Length == 10)
+                    {
+                        phone = new Smartphone();
+                    }
+                    else if (phoneNumber.Length == 7)
+                    {
+                        phone = new StationaryPhone();
+                    }
+                    else
+                    {
+                        throw new ArgumentException("Invalid number!");
+                    }
+
+                    result = phone.Call(phoneNumber);
+
+                }
+                catch (ArgumentException ex)
+                {
+                    result = ex.Message;
+                }
+
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
---
+++ C#/C# OOP/InterfacesAndAbstraction.Exercise/03.Telephony/Smartphone.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _03.Telephony
+{
+    public class Smartphone : ICallable, IBrowsable
+    {
+        public string Browse(string url)
+        {
+            return $"Browsing: {url}!";
+        }
+
+        public string Call(string phoneNumber)
+        {
+            return $"Calling... {phoneNumber}";
+        }
+    }
+}
