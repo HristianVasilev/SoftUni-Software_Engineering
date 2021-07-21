6f0a784cf73548c822dfbcaa691d9d5a014e0138
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-07-21 20:26:33 +0300

    Interfaces and abstraction
    Multiimplementation

---
+++ C#/C# OOP/InterfacesAndAbstraction.Exercise/02.MultipleImplementation/Citizen.cs
@@ -0,0 +1,38 @@
+﻿namespace PersonInfo
+{
+    public class Citizen : IPerson, IIdentifiable, IBirthable
+    {
+        private string name;
+        private int age;
+
+        public Citizen(string name, int age, string id, string birthdate)
+        {
+            Name = name;
+            Age = age;
+            Id = id;
+            Birthdate = birthdate;
+        }
+
+        public string Name
+        {
+            get => this.name;
+            private set
+            {
+                name = value;
+            }
+        }
+
+        public int Age
+        {
+            get => age;
+            private set
+            {
+                age = value;
+            }
+        }
+
+        public string Id { get; private set; }
+
+        public string Birthdate { get; private set; }
+    }
+}
