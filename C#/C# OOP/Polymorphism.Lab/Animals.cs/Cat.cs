5aa817f840f1fcf09fb2a8c4d3d7c53bee025385
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-07-25 10:46:56 +0300

    Animals

---
+++ C#/C# OOP/Polymorphism.Lab/Animals.cs/Cat.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+
+namespace Animals
+{
+    class Cat : Animal
+    {
+        private const string produceSound = "MEEOW";
+
+        public Cat(string name, string favouriteFood) : base(name, favouriteFood)
+        {
+        }
+
+        public override string ExplainSelf()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine(base.ExplainSelf());
+            sb.AppendLine(produceSound);
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
---
+++ C#/C# OOP/Polymorphism.Lab/Animals.cs/Dog.cs
@@ -0,0 +1,24 @@
+﻿using System.Text;
+
+namespace Animals
+{
+    class Dog : Animal
+    {
+        private const string produceSound = "DJAAF";
+
+
+        public Dog(string name, string favouriteFood) : base(name, favouriteFood)
+        {
+        }
+
+        public override string ExplainSelf()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine(base.ExplainSelf());
+            sb.AppendLine(produceSound);
+
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
