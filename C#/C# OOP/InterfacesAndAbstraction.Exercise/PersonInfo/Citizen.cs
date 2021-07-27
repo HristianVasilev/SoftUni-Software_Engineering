b5523789934669c51923b4ee4994f77101c3fd93
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-07-27 09:13:55 +0300

    Person info problem

---
+++ C#/C# OOP/InterfacesAndAbstraction.Exercise/PersonInfo/Citizen.cs
@@ -0,0 +1,32 @@
+﻿namespace PersonInfo
+{
+    public class Citizen : IPerson
+    {
+        private string name;
+        private int age;
+
+        public Citizen(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name
+        {
+            get => this.name;
+            private set
+            {
+                name = value;
+            }
+        }
+
+        public int Age
+        {
+            get => age;
+            private set
+            {
+                age = value;
+            }
+        }
+    }
+}
