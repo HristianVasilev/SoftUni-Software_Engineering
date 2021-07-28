2f66207ffb8ef3dba92c9f98b9343ff34a675755
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-07-28 21:44:38 +0300

    Binary tree

---
+++ DataStructures/Heaps.Lab/01.BinaryTree/BinaryTree.cs
@@ -0,0 +1,118 @@
+﻿namespace _01.BinaryTree
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    public class BinaryTree<T> : IAbstractBinaryTree<T>
+    {
+        public BinaryTree(T value
+            , IAbstractBinaryTree<T> leftChild
+            , IAbstractBinaryTree<T> rightChild)
+        {
+            this.Value = value;
+            this.LeftChild = leftChild;
+            this.RightChild = rightChild;
+        }
+
+        public T Value { get; private set; }
+
+        public IAbstractBinaryTree<T> LeftChild { get; private set; }
+
+        public IAbstractBinaryTree<T> RightChild { get; private set; }
+
+
+        public string AsIndentedPreOrder(int indent)
+        {
+            StringBuilder result = new StringBuilder();
+            result.Append($"{new string(' ', indent)}{this.Value}");
+            result.Append(Environment.NewLine);
+
+            if (this.LeftChild != null)
+            {
+                result.Append(this.LeftChild.AsIndentedPreOrder(indent + 2));
+            }
+
+            if (this.RightChild != null)
+            {
+                result.Append(this.RightChild.AsIndentedPreOrder(indent + 2));
+            }
+
+            return result.ToString();
+        }
+
+
+
+        public List<IAbstractBinaryTree<T>> InOrder()
+        {
+            List<IAbstractBinaryTree<T>> result = new List<IAbstractBinaryTree<T>>();
+
+            if (this.LeftChild != null)
+            {
+                result.AddRange(this.LeftChild.InOrder());
+            }
+
+            result.Add(this);
+
+            if (this.RightChild != null)
+            {
+                result.AddRange(this.RightChild.InOrder());
+            }
+
+            return result;
+        }
+
+        public List<IAbstractBinaryTree<T>> PostOrder()
+        {
+            List<IAbstractBinaryTree<T>> result = new List<IAbstractBinaryTree<T>>();
+
+            if (this.LeftChild != null)
+            {
+                result.AddRange(this.LeftChild.PostOrder());
+            }
+
+            if (this.RightChild != null)
+            {
+                result.AddRange(this.RightChild.PostOrder());
+            }
+
+            result.Add(this);
+
+            return result;
+        }
+
+        public List<IAbstractBinaryTree<T>> PreOrder()
+        {
+            List<IAbstractBinaryTree<T>> result = new List<IAbstractBinaryTree<T>>();
+
+            result.Add(this);
+
+            if (this.LeftChild != null)
+            {
+                result.AddRange(this.LeftChild.PreOrder());
+            }
+
+            if (this.RightChild != null)
+            {
+                result.AddRange(this.RightChild.PreOrder());
+            }
+
+            return result;
+        }
+
+        public void ForEachInOrder(Action<T> action)
+        {
+            if (this.LeftChild != null)
+            {
+                this.LeftChild.ForEachInOrder(action);
+            }
+
+            action.Invoke(this.Value);
+
+            if (this.RightChild != null)
+            {
+                this.RightChild.ForEachInOrder(action);
+            }
+        }
+    }
+}
