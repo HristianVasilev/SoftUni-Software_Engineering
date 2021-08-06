03591b79692069c6ced3c93ed3bc9df5d49e8574
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-08-06 17:44:57 +0300

    Create Program.cs

---
+++ C#/C# OOP/ReflectionAndAttributes.Lab/ReflectionAndAttributes.Lab/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ReflectionAndAttributes.Lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
