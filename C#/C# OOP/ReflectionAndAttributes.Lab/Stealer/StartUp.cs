4ec2e34f66695cb3f7bd8d3a78ed1b2c04539af0
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-08-07 18:41:35 +0300

    Create StartUp.cs

---
+++ C#/C# OOP/ReflectionAndAttributes.Lab/Stealer/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Stealer
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Spy spy = new Spy();
+            string result = spy.CollectGettersAndSetters("Stealer.Hacker");
+            // spy.RevealPrivateMethods("Stealer.Hacker");
+            //spy.AnalyzeAccessModifiers("Stealer.Hacker");
+            //spy.StealFieldInfo("Stealer.Hacker", "username", "password");
+
+            Console.WriteLine(result);
+        }
+    }
+}
