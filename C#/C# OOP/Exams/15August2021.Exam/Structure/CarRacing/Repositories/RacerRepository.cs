8ef9c965bfa508a945996f15fdc65d04a38a2a88
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-08-29 19:28:56 +0300

    Racer repository

---
+++ C#/C# OOP/Exams/15August2021.Exam/Structure/CarRacing/Repositories/RacerRepository.cs
@@ -0,0 +1,41 @@
+﻿namespace CarRacing.Repositories
+{
+    using CarRacing.Models.Racers.Contracts;
+    using CarRacing.Repositories.Contracts;
+    using CarRacing.Utilities.Messages;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class RacerRepository : IRepository<IRacer>
+    {
+        private ICollection<IRacer> models;
+
+        public RacerRepository()
+        {
+            this.models = new List<IRacer>();
+        }
+
+        public IReadOnlyCollection<IRacer> Models => this.models as IReadOnlyCollection<IRacer>;
+
+        public void Add(IRacer model)
+        {
+            if (model == null)
+            {
+                throw new ArgumentException(ExceptionMessages.InvalidAddRacerRepository);
+            }
+
+            this.models.Add(model);
+        }
+
+        public bool Remove(IRacer model)
+        {
+            return this.models.Remove(model);
+        }
+
+        public IRacer FindBy(string property)
+        {
+            return this.models.FirstOrDefault(r => r.Username == property);
+        }
+    }
+}
