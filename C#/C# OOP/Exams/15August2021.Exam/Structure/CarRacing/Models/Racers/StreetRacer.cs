cacc17eb5974c0a881bb262f6307d03e41af1a3c
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-08-29 19:31:02 +0300

    Create StreetRacer.cs

---
+++ C#/C# OOP/Exams/15August2021.Exam/Structure/CarRacing/Models/Racers/StreetRacer.cs
@@ -0,0 +1,20 @@
+﻿namespace CarRacing.Models.Racers
+{
+    using CarRacing.Models.Cars.Contracts;
+
+    public class StreetRacer : Racer
+    {
+        private const string racingBehavior = "aggressive";
+        private const int drivingExperience = 10;
+        private const int increasingDrivingExperiencePerRace = 5;
+
+        public StreetRacer(string username, ICar car)
+            : base(username, racingBehavior, drivingExperience, car) { }
+
+        public override void Race()
+        {
+            base.Race();
+            this.DrivingExperience += increasingDrivingExperiencePerRace;
+        }
+    }
+}
