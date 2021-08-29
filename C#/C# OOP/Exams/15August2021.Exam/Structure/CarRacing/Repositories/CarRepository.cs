0ca339941a9c15af567fae6c5b50f02a814d0c46
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-08-29 19:29:20 +0300

    Create CarRepository.cs

---
+++ C#/C# OOP/Exams/15August2021.Exam/Structure/CarRacing/Repositories/CarRepository.cs
@@ -0,0 +1,42 @@
+﻿namespace CarRacing.Repositories
+{
+    using CarRacing.Models.Cars.Contracts;
+    using CarRacing.Repositories.Contracts;
+    using CarRacing.Utilities.Messages;
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class CarRepository : IRepository<ICar>
+    {
+        private ICollection<ICar> models;
+
+        public CarRepository()
+        {
+            this.models = new List<ICar>();
+        }
+
+        public IReadOnlyCollection<ICar> Models => this.models as IReadOnlyCollection<ICar>;
+
+
+        public void Add(ICar model)
+        {
+            if (model == null)
+            {
+                throw new ArgumentException(ExceptionMessages.InvalidAddCarRepository);
+            }
+
+            this.models.Add(model);
+        }
+
+        public bool Remove(ICar model)
+        {
+            return this.models.Remove(model);
+        }
+
+        public ICar FindBy(string property)
+        {
+            return this.models.FirstOrDefault(c => c.VIN == property);
+        }
+    }
+}
