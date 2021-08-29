15b3601c707d98d6b55146132e276fbda5527c2d
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-08-29 19:48:34 +0300

    Create ProfessionalRacer.cs

---
+++ C#/C# OOP/Exams/15August2021.Exam/Structure/CarRacing/Models/Racers/ProfessionalRacer.cs
@@ -0,0 +1,20 @@
+﻿namespace CarRacing.Models.Racers
+{
+    using CarRacing.Models.Cars.Contracts;
+
+    public class ProfessionalRacer : Racer
+    {
+        private const string racingBehavior = "strict";
+        private const int drivingExperience = 30;
+        private const int increasingDrivingExperiencePerRace = 10;
+
+        public ProfessionalRacer(string username, ICar car)
+            : base(username, racingBehavior, drivingExperience, car) { }
+
+        public override void Race()
+        {
+            base.Race();
+            this.DrivingExperience += increasingDrivingExperiencePerRace;
+        }
+    }
+}
