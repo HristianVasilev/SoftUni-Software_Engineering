94a74ed16c065cdfa138f5102391ace4daa54477
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-09-01 18:33:50 +0300

    Create TunedCar.cs

---
+++ C#/C# OOP/Exams/15August2021.Exam/Structure/CarRacing/Models/Cars/TunedCar.cs
@@ -0,0 +1,20 @@
+﻿namespace CarRacing.Models.Cars
+{
+    using System;
+
+    public class TunedCar : Car
+    {
+        private const double fuelAvailable = 65;
+        private const double fuelConsumptionPerRace = 7.5;
+
+        public TunedCar(string make, string model, string VIN, int horsePower)
+            : base(make, model, VIN, horsePower, fuelAvailable, fuelConsumptionPerRace) { }
+
+        public override void Drive()
+        {
+            base.Drive();
+            this.HorsePower = (int)Math.Round(this.HorsePower - this.HorsePower * 0.03);
+        }
+
+    }
+}
