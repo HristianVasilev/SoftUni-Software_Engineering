77ea51e665342ff47348cbb0d3269c6e1eba2f94
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-09-01 19:31:21 +0300

    Create SuperCar.cs

---
+++ C#/C# OOP/Exams/15August2021.Exam/Structure/CarRacing/Models/Cars/SuperCar.cs
@@ -0,0 +1,15 @@
+﻿namespace CarRacing.Models.Cars
+{
+    public class SuperCar : Car
+    {
+        private const double fuelAvailable = 80;
+        private const double fuelConsumptionPerRace = 10;
+
+        public SuperCar(string make, string model, string VIN, int horsePower)
+            : base(make, model, VIN, horsePower, fuelAvailable, fuelConsumptionPerRace) 
+        {
+            this.Make = "Neznam";
+        }
+
+    }
+}
