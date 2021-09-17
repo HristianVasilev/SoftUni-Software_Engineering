e006195cee8d4a497f4333523d07182f9290e331
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-09-17 15:22:07 +0300

    Comparator by swag

---
+++ DataStructures/DataStructuresAdvanced/HashTabes.Exercise/01.RoyaleArena/Comparators/CompareBySwagThenById.cs
@@ -0,0 +1,17 @@
+﻿namespace _01.RoyaleArena
+{
+    using System.Collections.Generic;
+
+    class CompareBySwagThenById : IComparer<BattleCard>
+    {
+        public int Compare(BattleCard x, BattleCard y)
+        {
+            if (x.Swag.Equals(y.Swag))
+            {
+                return x.Id.CompareTo(y.Id);
+            }
+
+            return x.Swag.CompareTo(y.Swag);
+        }
+    }
+}
