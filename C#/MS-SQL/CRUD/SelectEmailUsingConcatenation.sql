b5d23e720706cacccb95ef66f441277bfe768478
Hristian Vasilev <hr.vasilev1@gmail.com>
2021-09-24 09:53:38 +0300

    Select queries

---
+++ C#/MS-SQL/CRUD/SelectEmailUsingConcatenation.sql
@@ -0,0 +1,10 @@
+USE SoftUni
+GO
+
+SELECT 
+	FirstName + '.' + LastName + '@softuni.bg' AS [Full Email Address]
+FROM Employees
+
+SELECT 
+	CONCAT(FirstName, '.', LastName, '@softuni.bg') AS [Full Email Address]
+FROM Employees
