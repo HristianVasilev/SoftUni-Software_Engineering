f4c986160eb1cdd5b38a60c37ce85697ecb6a873
Hristian Vasilev <hr.vasilev1@gmail.com>
2022-11-02 08:44:59 +0200

    Add new Prduct view model

---
+++ C#/Web/Web-Basics(Fundamentals)/MVC-Demo/MVC-Demo/Models/ProductViewModel.cs
@@ -0,0 +1,25 @@
+﻿namespace MVC_Demo.Models
+{
+    public class ProductViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        public ProductViewModel()
+        {
+        }
+
+        public ProductViewModel(int id, string name, double price)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+        }
+
+        public override string ToString()
+        {
+            return $"[Product] ID: {Id}, {Name} - {Price}lv.";
+        }
+    }
+}
