163ed036b8d1902a74a2df36a89b2da47f094de9
Hristian Vasilev <hr.vasilev1@gmail.com>
2022-11-02 08:45:25 +0200

    Add Products controller and extend Home controller

--- C#/Web/Web-Basics(Fundamentals)/MVC-Demo/MVC-Demo/Controllers/HomeController.cs
+++ C#/Web/Web-Basics(Fundamentals)/MVC-Demo/MVC-Demo/Controllers/HomeController.cs
@@ -15,6 +15,26 @@
 
         public IActionResult Index()
         {
+            ViewBag.Message = "Hello World!";
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            ViewBag.Message = "This is an ASP.NET Core MVC app.";
+            return View();
+        }
+
+        public IActionResult Numbers()
+        {
+            ViewBag.Message = "Numbers from 1 to 50";
+            return View();
+        }
+
+        public IActionResult NumbersToN(int count = 3)
+        {
+            ViewBag.Count = count;
+            ViewBag.Message = $"Numbers from 1 to {ViewBag.Count}";
             return View();
         }
 
---
+++ C#/Web/Web-Basics(Fundamentals)/MVC-Demo/MVC-Demo/Controllers/ProductsController.cs
@@ -0,0 +1,69 @@
+﻿namespace MVC_Demo.Controllers
+{
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.Net.Http.Headers;
+    using MVC_Demo.Models;
+    using System.Text;
+    using System.Text.Json;
+
+    public class ProductsController : Controller
+    {
+        private IEnumerable<ProductViewModel> products = new List<ProductViewModel>()
+        {
+            new ProductViewModel(205066,"Beer", 1.69),
+            new ProductViewModel(173044, "Pizza", 9.39),
+            new ProductViewModel(294033, "Ice cream", 2.09)
+        };
+
+        [ActionName("My-Products")]
+        public IActionResult All(string keyword)
+        {
+            IEnumerable<ProductViewModel> products = this.products;
+            if (keyword != null)
+            {
+                products = this.products.Where(pr => pr.Name.ToLower().Contains(keyword.ToLower()));
+            }
+            return View(products);
+        }
+
+        public IActionResult ById(int id)
+        {
+            var product = this.products.FirstOrDefault(p => p.Id.Equals(id));
+            if (product is null)
+            {
+                return BadRequest();
+            }
+            return View(product);
+        }
+
+        public IActionResult AllAsJson()
+        {
+            var jsonOptions = new JsonSerializerOptions
+            {
+                WriteIndented = true
+            };
+            return Json(products, jsonOptions);
+        }
+
+        public IActionResult AllAsText()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (var pr in this.products)
+            {
+                stringBuilder.AppendLine(pr.ToString());
+            }
+            return Content(stringBuilder.ToString().TrimEnd());
+        }
+
+        public IActionResult AllAsTextFile()
+        {
+            StringBuilder stringBuilder = new StringBuilder();
+            foreach (var pr in this.products)
+            {
+                stringBuilder.AppendLine(pr.ToString());
+            }
+            Response.Headers.Add(HeaderNames.ContentDisposition, @"attachment;filename=products.txt");
+            return File(Encoding.UTF8.GetBytes(stringBuilder.ToString().TrimEnd()), "text/plain");
+        }
+    }
+}
