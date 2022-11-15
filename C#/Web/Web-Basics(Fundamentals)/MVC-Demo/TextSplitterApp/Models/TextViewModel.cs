3719cdbcc4a31e52049e7e9a3be819ebbbba3ecc
Hristian Vasilev <hr.vasilev1@gmail.com>
2022-11-15 20:45:59 +0200

    Text view model

---
+++ C#/Web/Web-Basics(Fundamentals)/MVC-Demo/TextSplitterApp/Models/TextViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace TextSplitterApp.Models
+{
+    using System.Security.Principal;
+
+    public class TextViewModel
+    {
+        public string? Text { get; set; }
+        public string SplitText { get; set; }
+    }
+}
