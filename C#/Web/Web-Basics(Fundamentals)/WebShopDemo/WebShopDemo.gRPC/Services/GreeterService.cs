184ba0a086d5108ea5d6d3aa67e5d2c6b2bbe3a5
Hristian Vasilev <hr.vasilev1@gmail.com>
2022-11-16 12:51:30 +0200

    Add API and gRPC projects

---
+++ C#/Web/Web-Basics(Fundamentals)/WebShopDemo/WebShopDemo.gRPC/Services/GreeterService.cs
@@ -0,0 +1,22 @@
+namespace WebShopDemo.gRPC.Services
+{
+    using Grpc.Core;
+    using WebShopDemo.gRPC;
+
+    public class GreeterService : Greeter.GreeterBase
+    {
+        private readonly ILogger<GreeterService> _logger;
+        public GreeterService(ILogger<GreeterService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new HelloReply
+            {
+                Message = "Hello " + request.Name
+            });
+        }
+    }
+}
---
+++ C#/Web/Web-Basics(Fundamentals)/WebShopDemo/WebShopDemo.gRPC/Services/ProductGrpcService.cs
@@ -0,0 +1,32 @@
+﻿namespace WebShopDemo.gRPC.Services
+{
+    using Google.Protobuf.WellKnownTypes;
+    using Grpc.Core;
+    using System.Threading.Tasks;
+    using WebShopDemo.Core.Contracts;
+
+    public class ProductGrpcService : Product.ProductBase
+    {
+        private IProductService productService;
+
+        public ProductGrpcService(IProductService productService)
+        {
+            this.productService = productService;
+        }
+
+        public override async Task<ProductList> GetAll(Empty request, ServerCallContext context)
+        {
+            ProductList result = new ProductList();
+            var products = await productService.GetAll();
+
+            result.Items.AddRange(products.Select(p => new ProductItem()
+            {
+                Name = p.Name,
+                Id = p.Id.ToString(),
+                Price = (double)p.Price,
+                Quiantity = p.Quantity
+            }));
+            return result;
+        }
+    }
+}
