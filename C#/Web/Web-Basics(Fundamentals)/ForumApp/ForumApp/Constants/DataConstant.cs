3f23acf2b98e162370e899c66c5f6557813c070a
Hristian Vasilev <hr.vasilev1@gmail.com>
2022-11-17 14:59:19 +0200

    Data constants are added

---
+++ C#/Web/Web-Basics(Fundamentals)/ForumApp/ForumApp/Constants/DataConstant.cs
@@ -0,0 +1,40 @@
+﻿namespace ForumApp.Constants
+{
+    public static class DataConstant
+    {
+        /// <summary>
+        /// Post constants
+        /// </summary>
+        public static class Post
+        {
+            /// <summary>
+            /// Min title length
+            /// </summary>
+            public const int TitleMinLength = 10;
+
+            /// <summary>
+            /// Max title length
+            /// </summary>
+            public const int TitleMaxLength = 50;
+
+            /// <summary>
+            /// Min content length
+            /// </summary>
+            public const int ContentMinLength = 30;
+
+            /// <summary>
+            /// Max content length
+            /// </summary>
+            public const int ContentMaxLength = 1500;
+
+            /// <summary>
+            /// Error messages for Posts
+            /// </summary>
+            public static class ErrorMessage
+            {
+                public const string FieldLength = "{0} must be a string between {2} and {1} characters.";
+                public const string FieldRequired = "{0} is required!";
+            }
+        }
+    }
+}
