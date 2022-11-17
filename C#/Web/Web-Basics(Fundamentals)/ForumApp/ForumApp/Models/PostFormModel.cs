15e7d27fd262c1908b6ea4ff9852b7160ebd4932
Hristian Vasilev <hr.vasilev1@gmail.com>
2022-11-17 14:58:50 +0200

    Post form model is added for Add/Edit action for a post

---
+++ C#/Web/Web-Basics(Fundamentals)/ForumApp/ForumApp/Models/PostFormModel.cs
@@ -0,0 +1,27 @@
+﻿namespace ForumApp.Models
+{
+    using ForumApp.Data.Models;
+    using System.ComponentModel.DataAnnotations;
+    using static ForumApp.Constants.DataConstant.Post;
+    using static ForumApp.Constants.DataConstant.Post.ErrorMessage;
+
+    public class PostFormModel
+    {
+        public PostFormModel()
+        {
+        }
+        public PostFormModel(Post post)
+        {
+            Title = post.Title;
+            Content = post.Content;
+        }
+
+        [Required(ErrorMessage = FieldRequired)]
+        [StringLength(TitleMaxLength, MinimumLength = TitleMinLength, ErrorMessage = FieldLength)]
+        public string? Title { get; set; }
+
+        [Required(ErrorMessage = FieldRequired)]
+        [StringLength(ContentMaxLength, MinimumLength = ContentMinLength, ErrorMessage = FieldLength)]
+        public string? Content { get; set; }
+    }
+}
