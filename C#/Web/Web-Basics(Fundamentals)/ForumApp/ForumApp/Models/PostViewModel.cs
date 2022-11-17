45269f4e2a6f193e725d4ea1e33c850847e94cd6
Hristian Vasilev <hr.vasilev1@gmail.com>
2022-11-17 14:58:25 +0200

    Post view model is added to show all posts on the page

---
+++ C#/Web/Web-Basics(Fundamentals)/ForumApp/ForumApp/Models/PostViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace ForumApp.Models
+{
+    using ForumApp.Data.Models;
+
+    public class PostViewModel
+    {
+        public PostViewModel()
+        {
+        }
+        public PostViewModel(Post post)
+        {
+            Id = post.Id;
+            Title = post.Title;
+            Content = post.Content;
+        }
+
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? Content { get; set; }
+    }
+}
